5b0454da94a425ea95d9448dec42a30d363bce2d
coccojack <grotondococco@gmail.com>
2022-06-01 18:54:09 +0200

    chore: add little spacing

--- tests/test_binary_converter.py
+++ tests/test_binary_converter.py
@@ -1,6 +1,7 @@
 from src.binary_converter import binary_convert
 
-def test_binary_converter()->None:
-    assert binary_convert(3)=='0b11'
-    assert binary_convert(7)=='0b111'
-    assert binary_convert(15)=='0b1111'
+
+def test_binary_converter() -> None:
+    assert binary_convert(3) == '0b11'
+    assert binary_convert(7) == '0b111'
+    assert binary_convert(15) == '0b1111'
