a7a0deef821dfe201246323b54e90371033001a6
Domenico Blanco <domenicog.blanco@gmail.com>
2022-06-01 18:01:43 +0200

    feat: implements random & tests

---
+++ src/random_number.py
@@ -0,0 +1,4 @@
+from random import randint
+
+def get_random_number() -> int:
+    return randint(0, 2147483647)
---
+++ tests/test_random_number.py
@@ -0,0 +1,9 @@
+from src import random_number
+from pytest_mock import MockerFixture
+
+def test_get_random_number(mocker: MockerFixture) -> None:
+    mocker.patch.object(random_number, 'randint', return_value=0)
+    spy = mocker.spy(random_number, 'randint')
+
+    assert random_number.get_random_number() == 0
+    assert spy.spy_return == 0
