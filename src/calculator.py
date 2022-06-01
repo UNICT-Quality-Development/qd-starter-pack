26052eac64f5102aafdf26e053fd8a7ef95ec9fa
Carlo Spata <SPTCRL02E26H163H@studium.unict.it>
2022-06-01 18:52:44 +0200

    feat: implement calculator

---
+++ src/calculator.py
@@ -0,0 +1,26 @@
+def calculate(x:float,y:float,op:str) -> float:
+    match op:  
+        case "S": 
+            return x+y
+        case "D": 
+            return x-y
+        case "M": 
+            return x*y
+        case "d": 
+            if y == 0:
+              print("ERROR, IMPOSSIBLE TO DIVIDE BY ZERO!","\n","\n") 
+              return 0
+            else:
+              return x/y    
+
+"""
+# REMOVE FOR TESTING
+x = float(input("Inserisci un numero:"))
+y = float(input("Inserisci un numero:"))
+
+print()
+print("Sum: ", calculate(x,y,"S"))
+print("Difference: ", calculate(x,y,"D"))
+print("Multiplication: ", calculate(x,y,"M"))
+print("Division: ", calculate(x,y,"d"))
+"""
