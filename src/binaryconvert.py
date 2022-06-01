94703f20b9f308076826ad838ef1b6ccf8d146a0
Carmelo Bertolami <carmelobert29@gmail.com>
2022-06-02 00:10:25 +0200

    definition of function binary convert

--- src/binaryconvert.py
+++ src/binaryconvert.py
@@ -1,5 +1,24 @@
-
 #  Write a program that given a number as input convert it in binary.
 #  Output:
 #  Insert first number: 8
 #  The binary number is: 1000
+
+import math
+
+def ConvertiInBinario(n):
+    valore=" "
+    while n>0:
+        
+        if n%2==0:
+            valore='0'+valore
+            
+        else:
+            valore='1'+valore
+            
+        n=int(n/2)
+    return valore
+    
+if __name__ == "__main__":
+    n=int(input('inserisci il valore del numero da inserire:'))
+    print(ConvertiInBinario(int(n)))
+    
