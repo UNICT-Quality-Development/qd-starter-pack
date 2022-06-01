b387819895dfc8ae7aed8831ca54ba39ff75faa1
babilkebab <enzcal002@gmail.com>
2022-06-01 17:45:33 +0200

    feat: binary_converter

---
+++ src/binary_converter.py
@@ -0,0 +1,23 @@
+'''
+  Write a program that given a number as input convert it in binary.
+  Output:
+  Insert first number: 8
+  The binary number is: 1000
+
+'''
+
+import math
+
+def binary_converter(dec : int) -> str:
+    numbin : str = ""
+    i = int(math.log(dec, 2)+1)
+    while i > 0:
+        numbin = str(dec%2) + numbin
+        dec = dec//2
+        i -= 1
+    return numbin
+
+
+if __name__ == "__main__":
+    print(binary_converter(255))
+    
