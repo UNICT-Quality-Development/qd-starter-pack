d5f3d79549604be75fdfd75f95d9c3e949131a97
Angelo Iacona <iaconaangelo.ai@gmail.com>
2022-06-01 18:55:35 +0200

    refactor: change bios dict from function return value into const

--- src/ex3.py
+++ src/ex3.py
@@ -1,36 +1,28 @@
-import string
-
-
-# Fill dictionary with values
-def get_bios() -> dict:
-    map = {}
-    map['BarackObama'] = '44th President of the United States'
-    map['SandroPertini'] = 'Former President of the Italian Republic'
-    map['NelsonMandela'] = 'Former President of South Africa'
-    map['MahatmaGandhi'] = 'Bapu'
-    map['DonaldKnuth'] = 'Creator of LaTeX'
-    map['DennisRitchie'] = 'Creator of C'
-    return map
+BIOS = {
+    'BarackObama': '44th President of the United States',
+    'SandroPertini': 'Former President of the Italian Republic',
+    'NelsonMandela': 'Former President of South Africa',
+    'MahatmaGandi': 'Bapu',
+    'DonaldKnuth': 'Creator of LaTeX',
+    'DennisRitchie': 'Creator of C',
+}
 
 
 # Get name from user input
-def get_name() -> string:
+def get_name() -> str:
     return input('Enter a famous name+surname (ex. BarackObama): ')
 
 
 # Get bio if exist, error otherwise
-def get_description(name: string, map: dict) -> string:
-    return map[name] if name in map else'Invalid input! Please enter a good name!'
+def get_description(name: str) -> str:
+    return BIOS[name] if name in BIOS else 'Invalid input! Please enter a good name!'
 
 
-def main() -> None:
-    # Create dictionary and fill with values
-    bios = get_bios()
-
-    name = get_name()
-    # Print bio
-    print(get_description(name, bios))
+# def main() -> None:
+#     name = get_name()
+#     # Print bio
+#     print(get_description(name, BIOS))
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
