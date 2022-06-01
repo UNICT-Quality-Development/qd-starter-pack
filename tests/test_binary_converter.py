a8df3259604df741b91dd35b742c38428f1ab691
babilkebab <enzcal002@gmail.com>
2022-06-01 18:40:12 +0200

    feat: added unit test for binary_converter

---
+++ tests/test_binary_converter.py
@@ -0,0 +1,24 @@
+from typing import List
+import pytest
+from pytest_mock import MockerFixture
+from src.binary_converter import bin_conv
+from src import binary_converter
+
+
+tests: List[dict] = [
+    {"mock_return" : "110", "param" : 6},
+    {"mock_return" : "1111111", "param" : 127},
+    {"mock_return" : "11010010010011", "param" : 13459}
+]
+
+@pytest.mark.parametrize("test", tests)
+def test_binary(mocker: MockerFixture, test: dict) -> None:
+    #arrange
+    mocker.patch.object(binary_converter, "bin_conv", return_value = test["param"])
+
+    #act
+    ret : str = bin_conv(test["param"])
+
+    #assert
+    assert (ret == test["mock_return"]) is True
+
