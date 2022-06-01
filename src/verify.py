e39a96ef40e2359f52999aad750bfc3044b3f315
Domenico Blanco <domenicog.blanco@gmail.com>
2022-06-01 18:04:26 +0200

    fix: avoid double calculation

--- src/verify.py
+++ src/verify.py
@@ -1,8 +1,9 @@
 def verify(number_to_search: int) -> bool:
     static_array = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+    number_is_present = number_to_search in static_array
 
-    should_add_not = '' if number_to_search in static_array else 'not '
+    should_add_not = '' if number_is_present else 'not '
 
     print(f'The number {number_to_search} is {should_add_not}present in the array.')
-    return number_to_search in static_array
+    return number_is_present
 
