e9218b2f782e80ca47c86655fc0e1e5664d2b6fe
Giovanni Maria Manduca <425gioviok@gmail.com>
2022-06-02 20:20:40 +0200

    chore: Polished match case, removed print

--- src/ex4.py
+++ src/ex4.py
@@ -1,32 +1,12 @@
 #Surprise!
 def ex4(month : int) -> str:
-    month_len = ""
     match month:
-        case 1:
-            month_len = "31 days"
+        case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+            return "31 days"
         case 2:
-            month_len = "28/29 days"
-        case 3:
-            month_len = "31 days"
-        case 4:
-            month_len = "30 days"
-        case 5:
-            month_len = "31 days"
-        case 6:
-            month_len = "30 days"
-        case 7:
-            month_len = "31 days"
-        case 8:
-            month_len = "31 days"
-        case 9:
-            month_len = "30 days"
-        case 10:
-            month_len = "31 days"
-        case 11:
-            month_len = "30 days"
-        case 12:
-            month_len = "31 days"
+            return "28/29 days"
+        case 4 | 6 | 9 | 11:
+            return "30 days"
         case _:
-            month_len = "Invalid input! Please enter month number between 1-12"
-    print(month_len)
-    return month_len
+            return "Invalid input! Please enter month number between 1-12"
+     
