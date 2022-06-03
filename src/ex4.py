401a637df82ce6d0d3d7d3a45bc1fe6e82d6fe9a
babilkebab <enzcal002@gmail.com>
2022-06-03 16:16:52 +0200

    feat: ex4 code is more readable

--- src/ex4.py
+++ src/ex4.py
@@ -2,4 +2,9 @@
 
 def months(monthnum: int) -> str:
     with31days : list[int] = [1,3,5,7,8,10,12]
-    return "31 days" if monthnum in with31days else ("28-29 days" if monthnum == 2 else "30 days")
+    if monthnum in with31days:
+        return "31 days"
+    else:
+        if monthnum == 2:
+            return "28-29 days"
+        return "30 days"
