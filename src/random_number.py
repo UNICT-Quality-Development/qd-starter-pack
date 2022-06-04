a26b356d16ec661b931d3ff4714a1bf7ba7d19f1
Domenico Blanco <domenicog.blanco@gmail.com>
2022-06-04 16:38:02 +0200

    improvement: default value & more detailed test

--- src/random_number.py
+++ src/random_number.py
@@ -1,4 +1,4 @@
 from random import randint
 
-def get_random_number(max_value: int) -> int:
+def get_random_number(max_value: int = 2147483647) -> int:
     return randint(0, max_value)
--- tests/test_random_number.py
+++ tests/test_random_number.py
@@ -5,5 +5,7 @@ def test_get_random_number(mocker: MockerFixture) -> None:
     mocker.patch.object(random_number, 'randint', return_value=0)
     spy = mocker.spy(random_number, 'randint')
 
+    assert random_number.get_random_number(100) == 0
     assert random_number.get_random_number() == 0
     assert spy.spy_return == 0
+    assert spy.call_count == 2
