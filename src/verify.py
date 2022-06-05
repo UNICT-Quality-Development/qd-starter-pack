fce294055bc0d1adb88c87db8269ed2193ae2e77
Carlo Spata <SPTCRL02E26H163H@studium.unict.it>
2022-06-05 11:54:53 +0200

    chor: add type to list, change function name & exclude main from coverage

--- src/verify.py
+++ src/verify.py
@@ -1,12 +1,15 @@
 from typing import List
-def isThere(arr:list,val:int,N:int) -> bool:
+def is_there(arr:list[int],val:int,N:int) -> bool:
     for i in range(N):
         if arr[i] == val:
             return True
         
     return False
 
-if __name__ == "__main__":
+def main(): # pragma: no cover
     arr = [3,4,5,1,2,3,4,9,13,0]
     num = int(input("Insert a number: "))
-    print("The number",num, "is" if isThere(arr,num,len(arr)) is True else "is not","present in the array!")
+    print("The number",num, "is" if is_there(arr,num,len(arr)) else "is not","present in the array!")    
+
+if __name__ == "__main__": # pragma: no cover
+    main()    
