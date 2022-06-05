5d9a4a33e48508379800f55644892087c68526ca
Carlo Spata <SPTCRL02E26H163H@studium.unict.it>
2022-06-05 11:52:05 +0200

    chor: change function name

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,9 +1,9 @@
-from src.verify import isThere
+from src.verify import is_there
 
 def test_verify() -> None:
     arr = [3,4,5,1,2,3,4,9,13,0]
-    assert isThere(arr,3,len(arr)) is True
-    assert isThere(arr,20,len(arr)) is False
-    assert isThere(arr,4,len(arr)) is True
-    assert isThere(arr,35,len(arr)) is False
+    assert is_there(arr,3,len(arr))
+    assert is_there(arr,20,len(arr)) is False
+    assert is_there(arr,4,len(arr))
+    assert is_there(arr,35,len(arr)) is False
     
