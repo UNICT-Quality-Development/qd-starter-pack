c39a48fcf4a01efcec5413d2f0c12033dfe88e39
Carmelo Seminara <carmelosem18@gmail.com>
2022-06-05 20:33:41 +0200

    feat: modifiche al programma

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,4 @@
-
-# Write a program that given two numbers as input make the main operations.
-# Output:
-# Insert first number: 4
-# Insert second number: 2
-# SUM: 6
-# Difference: 2
-# Multiplication: 8
-# Division: 2
+from random import randint
 
 def sum(a, b):
     return a + b
@@ -20,13 +12,33 @@ def multiplication(a, b):
 def division(a, b):
     return a / b    
 
-print("Insert first number:")
-first_number = float ( input() )
+def first_number():
+    print("Insert first number:")
+    first_number = float ( input() )
 
-print("Insert second number:")
-second_number = float ( input() )
+    return first_number
 
-print( "sum:" , sum(first_number, second_number) )
-print( "difference:" , difference(first_number, second_number) )
-print( "multiplication:" , multiplication(first_number, second_number) )
-print( "division:" , division(first_number, second_number) )
+def second_number():
+    print("Insert second number:")
+    second_number = float ( input() )   
+
+    return second_number
+     
+def calculate():  
+    first_num = first_number()
+    second_num = second_number()  
+    random_num = randint(0, 3)
+
+    match random_num:  
+        case 0: 
+            sum_result = sum(first_num, second_num)
+            return sum_result
+        case 1: 
+            difference_result = difference(first_num, second_num)
+            return difference_result
+        case 2: 
+            multiplication_result = multiplication(first_num, second_num) 
+            return multiplication_result
+        case 3: 
+            division_result = division(first_num, second_num) 
+            return division_result  
