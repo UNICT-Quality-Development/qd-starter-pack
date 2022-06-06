0fdab44c3e467005ee45976a9999458957f47bbd
Carmelo Seminara <carmelosem18@gmail.com>
2022-06-06 18:19:52 +0200

    chore: miglioramento leggibilità del codice

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,30 +4,29 @@ from pytest_mock import MockerFixture
 from src import calculator
 
 tests: List[dict] = [
-{ "mock_value_1": 10, "mock_function_1": "first_number", 
-    "mock_value_2": 20, "mock_function_2": "second_number", 
-    "mock_value_3": 0, "mock_function_3": "randint", "res": calculator.calculate, "expected_res": 30 } ,
+{ "value_1": 10, "function_1": "first_number", 
+    "value_2": 20, "function_2": "second_number", 
+    "value_3": 0, "function_3": "randint", "res": calculator.calculate, "expected_res": 30 } ,
 
-    { "mock_value_1": 10, "mock_function_1": "first_number", 
-    "mock_value_2": 20, "mock_function_2": "second_number", 
-    "mock_value_3": 1, "mock_function_3": "randint", "res": calculator.calculate, "expected_res": -10 } ,   
+    { "value_1": 10, "function_1": "first_number", 
+    "value_2": 20, "function_2": "second_number", 
+    "value_3": 1, "function_3": "randint", "res": calculator.calculate, "expected_res": -10 } ,   
 
-    { "mock_value_1": 10, "mock_function_1": "first_number", 
-    "mock_value_2": 20, "mock_function_2": "second_number", 
-    "mock_value_3": 2, "mock_function_3": "randint", "res": calculator.calculate, "expected_res": 200 } ,
+    { "value_1": 10, "function_1": "first_number", 
+    "value_2": 20, "function_2": "second_number", 
+    "value_3": 2, "function_3": "randint", "res": calculator.calculate, "expected_res": 200 } ,
 
-    { "mock_value_1": 10, "mock_function_1": "first_number", 
-    "mock_value_2": 20, "mock_function_2": "second_number", 
-    "mock_value_3": 3, "mock_function_3": "randint", "res": calculator.calculate, "expected_res": 0.5 } ,    
+    { "value_1": 10, "function_1": "first_number", 
+    "value_2": 20, "function_2": "second_number", 
+    "value_3": 3, "function_3": "randint", "res": calculator.calculate, "expected_res": 0.5 } ,    
 ]
 
 @pytest.mark.parametrize("test", tests)
 def test_generic(mocker: MockerFixture, test:dict) -> None:
     # arrange
-
-    mocker.patch.object(calculator, test["mock_function_1"] , return_value = test["mock_value_1"])
-    mocker.patch.object(calculator, test["mock_function_2"] , return_value = test["mock_value_2"])
-    mocker.patch.object(calculator, test["mock_function_3"], return_value = test["mock_value_3"])
+    mocker.patch.object(calculator, test["function_1"] , return_value = test["value_1"])
+    mocker.patch.object(calculator, test["function_2"] , return_value = test["value_2"])
+    mocker.patch.object(calculator, test["function_3"], return_value = test["value_3"])
 
     # act
     res = test["res"]()
