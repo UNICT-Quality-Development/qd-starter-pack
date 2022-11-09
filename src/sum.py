a30edea40e89eef9eb53e3b5d769cc3fa4029748
mattiapuglia <101093169+mattiapuglia@users.noreply.github.com>
2022-11-09 18:19:02 +0100

    fix: prevent execution from importing and minor changes

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,8 @@
-def sum(a: int, b: int) -> int:
+def sum(a: float, b: float) -> float:
     return a + b
+if __name__=="__main__":
+    print("Insert the first number")
+    n1 = float(input())
+    print("Insert the second number")
+    n2 = float(input())
+    print("The sum is", sum(n1, n2))
