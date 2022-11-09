d925db06338066474b5d9b6a4ba6d4a296852af8
Luca Strano <stranoluca469955@gmail.com>
2022-11-09 18:40:38 +0100

    chore: resolve pipeline problems

--- src/verify.py
+++ src/verify.py
@@ -1,9 +1,10 @@
 if __name__ == "__main__":
-    
+
     N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
 
     number = int(input("Insert number: "))
 
-    if number in N: print(f"The number {number} is present in the array")
-    else: print(f"The number {number} is not present in the array")
-    
+    if number in N: 
+        print(f"The number {number} is present in the array")
+    else: 
+        print(f"The number {number} is not present in the array")
