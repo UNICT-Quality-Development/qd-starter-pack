5b9ca7367a64183284199e7511a5ed0980465b95
Melo Fuccio <carmelofuccio22@gmail.com>
2022-11-09 17:46:48 +0100

    feat: week(1-7)

---
+++ src/week.py
@@ -0,0 +1,24 @@
+def week():
+    x = int(input("Enter week number (1-7) \n "))
+    if x == 1 :
+        print("Monday")
+    elif x == 2:
+        print("Tuesday")
+    elif x == 3:
+        print ("Wednesday")
+    elif x == 4:
+        print("Thursday")
+    elif x == 5:
+        print("Friday")
+    elif x == 6:
+        print("Saturday")
+    elif x == 7:
+        print("Sunday")
+
+week()
+
+
+    
+
+    
+    
