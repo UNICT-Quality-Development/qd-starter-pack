c09efc81dbbee80e1104211e9ae31d715005dbd9
Bianca Giorgio <biancagiorgio987@gmail.com>
2022-11-09 18:18:26 +0100

    Versione 1.0 del file sum.py

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,7 @@
-def sum(a: int, b: int) -> int:
+def sum_int(a: int, b: int) -> int:
     return a + b
+
+n1=int(input("Inserire il primo numero: "))
+n2=int(input("Inserire il secondo numero: "))
+print("La somma tra "+str(n1)+"+"+str(n2)+"="+str(sum_int(n1,n2)))
+
