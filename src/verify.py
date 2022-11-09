7c487cad44fcc7b28376c86eec739262543e7ff4
Andrei Stefan <andry4you@hotmail.it>
2022-11-09 17:41:35 +0100

    feat: verify definition

---
+++ src/verify.py
@@ -0,0 +1,10 @@
+# Write a software that verifies if a number is present in a pre-defined array
+
+numbers = ('1','2','3','4','5','6','8','9','10')
+
+a = input("Insert a number: ")
+
+if a in numbers:
+    print("The number is in the list of numbers")
+else:
+    print("The number isn't in the list of numbers")
