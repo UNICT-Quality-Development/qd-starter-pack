5387c5355ac1cdda1b10da8c9462c03317902fce
Davide496 <milazzodavidesc@gmail.com>
2022-11-09 18:13:43 +0100

    chore: minor changes

--- src/ex1.py
+++ src/ex1.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     while value < 1 or value > 7:
         print(f'{value} is not a valid value\n')
         value = int(input("Insert a number (from 1 to 7): "))
+        break
     else:
         print(f'Day selected: {weeks[value-1]}')
+
