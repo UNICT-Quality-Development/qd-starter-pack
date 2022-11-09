2ff9ed971155fae3519c1a40217a67cdcff66f88
Andrei Stefan <andry4you@hotmail.it>
2022-11-09 18:18:19 +0100

    feat: add main's if

--- src/sum.py
+++ src/sum.py
@@ -1,7 +1,8 @@
 def sum(a: float, b: float) -> float:
     return a+b
 
-number1 = float(input("Choose the first number: "))
-number2 = float(input("Choose the second number: "))
+if __name__ == "__main__":
+    number1 = float(input("Choose the first number: "))
+    number2 = float(input("Choose the second number: "))
 
-print("Sum = ", sum(number1,number2))
+    print("Sum = ", sum(number1,number2))
