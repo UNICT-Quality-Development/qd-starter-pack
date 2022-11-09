c2dacd310d362dff01e2b22f03da482657f6efa9
Salvatore <salvatoredipietro35@gmail.com>
2022-11-09 17:47:47 +0100

    modifed the input and the print of the sum

--- src/summ_def.py
+++ src/summ_def.py
@@ -1,8 +1,8 @@
-numb1 = input('Insert the first numb')
-numb2 = input('Insert the second numb')
+numb1 = input('Insert the first numb: ')
+numb2 = input('Insert the second numb: ')
 
 s = int(0)
 
 s = numb1 + numb2
 
-print('The sum is {s}')
+print('The sum is: '+ s)
