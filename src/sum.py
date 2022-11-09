7a8f01cf24e78cc5eb22e02f09ec8124c9ddec07
Bianca Giorgio <biancagiorgio987@gmail.com>
2022-11-09 18:20:36 +0100

    Effettuato il controllo dell'input

--- src/sum.py
+++ src/sum.py
@@ -1,7 +1,11 @@
 def sum_int(a: int, b: int) -> int:
     return a + b
 
-n1=int(input("Inserire il primo numero: "))
-n2=int(input("Inserire il secondo numero: "))
+def controllo(mess: list)->int:
+    n=""
+    while(n.isdecimal()==False): n=input(mess)
+    return int(n)
+n1=controllo("Inserire il primo numero: ")
+n2=controllo("Inserire il secondo numero: ")
 print("La somma tra "+str(n1)+"+"+str(n2)+"="+str(sum_int(n1,n2)))
 
