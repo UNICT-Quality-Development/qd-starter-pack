40cf528c502417980951ac375d6e582cd73695e2
Giancarmelo <giancarmelo99@gmail.com>
2022-11-09 17:31:02 +0100

    feat: create random number

---
+++ src/random_number.py
@@ -0,0 +1,11 @@
+# Write a program that generates a random number.
+# Output:
+# The random number is: 4
+import random;
+
+def random_number() -> int:
+  return random.randint(0,400) 
+
+
+print("The random number id: " + str( random_number() ))
+  
