ba90471e7b916afc5d302c22432162d23c7dde41
Salvatore <salvatoredipietro35@gmail.com>
2022-11-09 18:12:41 +0100

    feat: added variables of input, variables that will contian results of operations and added print of the sum, difference and multiplication

--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,11 @@
-numb1 = int(input('Insert the first number: ')) #declare the variable numb1 and initialized it how input
-numb2 = int(input('Insert the second number: ')) #declare the variable numb2 and initialized it how input
+numb1 = float(input('Insert the first number: ')) #declare the variable numb1 and initialized it how input
+numb2 = float(input('Insert the second number: ')) #declare the variable numb2 and initialized it how input
+
+s = numb1 + numb2
+m = numb1 * numb2
+d = numb1 / numb2
+st = numb1 - numb2
+
+print(f'SUM: {s}')
+print(f'Difference: {st}')
+print(f'Multiplication: {m}')
