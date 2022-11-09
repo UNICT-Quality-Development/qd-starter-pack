cc5c3c49c00b654c604414cdc7e721a7391f2c42
Luca Strano <stranoluca469955@gmail.com>
2022-11-09 18:18:01 +0100

    chore: resolve pipeline problem

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -1,13 +1,13 @@
 def dec_bin(num: int) -> int:
     binary = ''
     while num>0:
-        if num%2 == 0:
+        if (num % 2) == 0:
             binary = '0' + binary
         else:
             binary = '1' + binary
-        num = int(num/2)
+        num = int(num / 2)
     return binary
 
 if __name__ == "__main__":
     number = int(input("Insert first number: "))
-    print("The binary number is: "+dec_bin(number))
+    print("The binary number is: " + dec_bin(number))
