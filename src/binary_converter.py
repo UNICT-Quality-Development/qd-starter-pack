c69f457631494a9d2b6c018727fa51d5c8b15871
Luca Strano <stranoluca469955@gmail.com>
2022-11-09 17:37:43 +0100

    feat: implemented a function dec_bin

---
+++ src/binary_converter.py
@@ -0,0 +1,13 @@
+def dec_bin(num: int) -> int:
+    binary = ''
+    while num>0:
+        if num%2 == 0:
+            binary = '0' + binary
+        else:
+            binary = '1' + binary
+        num = int(num/2)
+    return binary
+
+number = int(input("Insert first number: "))
+
+print("The binary number is: "+dec_bin(number))
