936c8071a6834124d1d8fc42905cfe4b51141002
Giancarmelo <giancarmelo99@gmail.com>
2022-11-09 18:39:36 +0100

    feat: add logic search

---
+++ src/verify.py
@@ -0,0 +1,14 @@
+# Write a software that verifies if a number is present in a pre-defined array.
+# Output example:
+# Insert number 3
+# The number 3 is [not] present in the array.
+
+if __name__ == '__main__':
+  N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+
+  value = input('Insert number: ')
+
+  if ( int( value ) in N ): 
+    print(f"The number {value} is  present in the array.")
+  else: 
+    print(f"The number {value} is not present in the array.")
