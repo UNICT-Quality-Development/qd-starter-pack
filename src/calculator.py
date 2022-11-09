6be6a861f7b4cbbdc7b4a8938ee1712959ea4657
Salvatore <salvatoredipietro35@gmail.com>
2022-11-09 18:20:13 +0100

    feat: add comment

--- src/calculator.py
+++ src/calculator.py
@@ -1,15 +1,15 @@
 numb1 = float(input('Insert the first number: ')) #declare the variable numb1 and initialized it how input
 numb2 = float(input('Insert the second number: ')) #declare the variable numb2 and initialized it how input
 
-s = numb1 + numb2
-m = numb1 * numb2
-d = numb1 / numb2
-st = numb1 - numb2
+s = numb1 + numb2 #do the sum of  two numbers
+m = numb1 * numb2 #do the multiplication of  two numbers
+d = numb1 / numb2 #do the division of  two numbers
+st = numb1 - numb2 #do the differce of two numbers
 
-print()
-print()
+print() #print empty space
+print() #print empty space
 
-print(f'SUM: {s}')
-print(f'Difference: {st}')
-print(f'Multiplication: {m}')
-print(f'Division: {d}')
+print(f'SUM: {s}') #print the sum
+print(f'Difference: {st}') #print the difference
+print(f'Multiplication: {m}') #print the multiplication
+print(f'Division: {d}') #print the division
