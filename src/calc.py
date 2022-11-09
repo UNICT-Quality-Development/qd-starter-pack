977dd64370c9bf7ff09e5ac08da136055d1bbcc0
fabriziosciolto <guiltybrizio@gmail.com>
2022-11-09 18:10:30 +0100

    feat: sum implemented

---
+++ src/calc.py
@@ -0,0 +1,18 @@
+"""
+  Write a program that given two numbers as input make the main operations.
+  Output:
+  Insert first number: 4
+  Insert second number: 2
+  SUM: 6
+  Difference: 2
+  Multiplication: 8
+  Division: 2
+"""
+print('insert first number: ')
+num1=input()
+print('insert second number: ')
+num2=input()
+
+print('somma: ')
+print(num1+num2)
+
