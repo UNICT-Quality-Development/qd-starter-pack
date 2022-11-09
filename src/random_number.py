cbd2f669536dfc4162c2acce2fde8e97c25064e8
Giancarmelo <giancarmelo99@gmail.com>
2022-11-09 17:37:33 +0100

    chore: add __name__ condiction

--- src/random_number.py
+++ src/random_number.py
@@ -6,6 +6,6 @@ import random;
 def random_number() -> int:
   return random.randint(0,400) 
 
-
-print("The random number id: " + str( random_number() ))
+if __name__ == "__main__":
+  print("The random number id: " + str( random_number() ))
   
