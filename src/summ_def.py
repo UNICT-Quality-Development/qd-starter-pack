8e75174f8f355f55223ff9d9997f9e51fee875de
Salvatore <salvatoredipietro35@gmail.com>
2022-11-09 18:00:56 +0100

    add comment

--- src/summ_def.py
+++ src/summ_def.py
@@ -1,6 +1,6 @@
-numb1 = int(input('Insert the first number: '))
-numb2 = int(input('Insert the second number: '))
+numb1 = int(input('Insert the first number: ')) #declare the variable numb1 and initialized it how input
+numb2 = int(input('Insert the second number: ')) #declare the variable numb2 and initialized it how input
 
-s = numb1 + numb2
+s = numb1 + numb2 #declare the varibale s to contain the sum and initialized it with the sum of numb1 and numb2
 
-print(f'The sum is: {s}')
+print(f'The sum is: {s}') #print the sum
