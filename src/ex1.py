510e673fd559d389561c3cf932c5c1967e2ab4b9
Tommaso Dirosa <DRSTMS00L12B428D@studium.unict.it>
2022-11-09 17:37:45 +0100

    ex1.py implementation

---
+++ src/ex1.py
@@ -0,0 +1,22 @@
+
+
+week = input("Enter week number(1-7): ")
+
+match week:
+    case "1":
+        print("Monday")
+    case "2":
+        print("Tuesday")
+    case "3":
+        print("Wednesday")
+    case "4":
+        print("Thursday")
+    case "5":
+        print("Friday")
+    case "6":
+        print("Saturday")
+    case "7":
+        print("Sunday")
+    case _:
+        print("Invalid input! Please enter week number between 1-7")
+    
