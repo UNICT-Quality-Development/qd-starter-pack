98a47cb3d3a06ba8e31a05a3d823d30eea6a6be6
Andrei Stefan <andry4you@hotmail.it>
2022-11-09 17:32:53 +0100

    feat: sum definition

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,7 @@
-def sum(a: int, b: int) -> int:
-    return a + b
+def sum(a:float, b:float) -> float:
+    return a+b
+
+a = float(input("Choose the first number: "))
+b = float(input("Choose the second number: "))
+
+print("Sum = ", sum(a,b))
