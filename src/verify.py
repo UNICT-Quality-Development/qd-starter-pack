a5e6ee307393a0093f850420273f6c64821de4ae
Real96 <chicco.gaeta@gmail.com>
2022-11-09 17:34:08 +0100

    feat: added verify.py

---
+++ src/verify.py
@@ -0,0 +1,11 @@
+'''
+Write a software that verifies if a number is present in a pre-defined array.
+Output example:
+Insert number 3
+The number 3 is [not] present in the array.
+'''
+
+N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+
+num = int(input("Insert number "))
+print(f'The number {num} is {"not" if num not in N else ""} present in the array')
