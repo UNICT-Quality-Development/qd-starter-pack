6faa569cad96ffbd6b93dfdfa0c1582ab9633fb1
Andrei Stefan <andry4you@hotmail.it>
2022-11-09 18:16:39 +0100

    feat: add verify function and if control

--- src/verify.py
+++ src/verify.py
@@ -1,10 +1,13 @@
 # Write a software that verifies if a number is present in a pre-defined array
 
-numbers = ('1','2','3','4','5','6','8','9','10')
 
-a = input("Insert a number: ")
+def verify(numbers: tuple, number: int) -> bool:
+    check = "" if number in numbers else " not"
+    print(f"The number is{check} in the list of numbers")
 
-if a in numbers:
-    print("The number is in the list of numbers")
-else:
-    print("The number isn't in the list of numbers")
+if __name__ == "__main__":
+    list_numbers = ('1','2','3','4','5','6','7','8','9','10')
+
+    number = input("Insert a number: ")
+
+    verify(list_numbers,number)
