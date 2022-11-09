60eca1f4e27eee9b465fefb76b66cbc6bb8f9e2a
Salvatore <salvatoredipietro35@gmail.com>
2022-11-09 18:07:57 +0100

    feat: add the file on the brunch

---
+++ src/calculator.py
@@ -0,0 +1,2 @@
+numb1 = int(input('Insert the first number: ')) #declare the variable numb1 and initialized it how input
+numb2 = int(input('Insert the second number: ')) #declare the variable numb2 and initialized it how input
