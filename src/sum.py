4913bfbe7c3a0188fb9e992e5dd54a0169baa9d3
mattiapuglia <101093169+mattiapuglia@users.noreply.github.com>
2022-11-09 17:51:10 +0100

    feat: implemented sum function

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,8 @@
-def sum(a: int, b: int) -> int:
+def sum(a: float, b: float) -> float:
     return a + b
+
+print("Insert the first number")
+a = float(input())
+print("Insert the second number")
+b = float(input())
+print("The sum is", sum(a,b))
