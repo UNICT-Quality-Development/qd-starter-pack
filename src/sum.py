82153991611ec2d136c7bdfabda70e39bccd7805
weelics <weelixhill@gmail.com>
2022-11-09 17:39:21 +0100

    chore: add user input

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,8 @@
-def sum(a: int, b: int) -> int:
+def sum(a:int, b:int) -> int:
     return a + b
+
+print("Insert the first number:")
+a =  input() 
+print("Insert the second number:")
+b = input()
+print("sum: " +  str(sum(int(a),int(b))))
