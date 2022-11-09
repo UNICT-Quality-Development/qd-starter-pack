711488da89fde34e42ce866dd27d90721c4722c5
Mary <cioacmaria@gmail.com>
2022-11-09 22:06:39 +0100

    exercise 1

---
+++ src/ex1.py
@@ -0,0 +1,28 @@
+#Improve this program using a switch-case. 
+week=int(input("Insert a number from 1 to 7: "))
+    
+match week: 
+    case 1:
+        print("It's Monday")
+    case 2:
+        print("It's Tuesday")
+    case 3:
+        print("It's Wednesday")
+    case 4:
+        print("It's Thursday")
+    case 5:
+        print("It's Friday")
+    case 6:
+        print("It's Saturday")
+    case 7:
+        print("It's Sunday")
+    case default:
+        print("There isn't any day with this number.")
+
+
+
+
+
+    
+
+   
