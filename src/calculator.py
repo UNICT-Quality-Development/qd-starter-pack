8df2b533f8ad8e9d387a0e4f14e0a898006ee619
michelanicastro <x81000889@studium.unict.it>
2022-11-09 18:10:29 +0100

    feat: sum implemented

---
+++ src/calculator.py
@@ -0,0 +1,19 @@
+"""
+  Write a program that given two numbers as input make the main operations.
+  Output:
+  Insert first number: 4
+  Insert second number: 2
+  SUM: 6
+  Difference: 2
+  Multiplication: 8
+  Division: 2
+"""
+
+print('Insert first number: ')
+n=input()
+
+print('Insert second number: ')
+m=input()
+
+print('Sum: ')
+print (n+m)
