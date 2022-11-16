8e7bc088affac2d80957213d310b150aacfc6f10
Giancarmelo <giancarmelo99@gmail.com>
2022-11-16 17:26:45 +0100

    chore: create function for verify

--- src/verify.py
+++ src/verify.py
@@ -3,11 +3,14 @@
 # Insert number 3
 # The number 3 is [not] present in the array.
 
-if __name__ == '__main__':
-  N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
-
+def verify(N: list[int] ):
   value = input('Insert number: ')
   if ( value.isnumeric() and int( value ) in N ):
     print(f"The number {value} is  present in the array.")
   else:
     print(f"The number {value} is not present in the array.")
+
+if __name__ == '__main__':
+  N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+  verify(N)
+  
