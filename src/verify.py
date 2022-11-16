a9ed0cf41fa28b610e57a62e76ef947ebbdd58b8
Giancarmelo <giancarmelo99@gmail.com>
2022-11-16 17:38:36 +0100

    feat: update verify function

--- src/verify.py
+++ src/verify.py
@@ -4,13 +4,12 @@
 # The number 3 is [not] present in the array.
 
 def verify(value_list: list[int], value: int ):
+  return int( value ) in value_list
 
-  if ( int( value ) in value_list ):
+if __name__ == '__main__':
+  value_list = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+  value = 3
+  if verify(value_list, value):
     print(f"The number {value} is  present in the array.")
   else:
     print(f"The number {value} is not present in the array.")
-
-if __name__ == '__main__':
-  N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
-  verify(N, 3)
-  
