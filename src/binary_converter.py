3156885ae4c9cfacc2835b3ba2f0ea772c7baa02
Neos <cappellotancredi@gmail.com>
2022-11-16 17:52:01 +0100

    feat: binary_converter

---
+++ src/binary_converter.py
@@ -0,0 +1,2 @@
+def to_binary(n, bin = bin):
+    return int(bin(n)[2:])
---
+++ tests/test_binary_converter.py
@@ -0,0 +1,7 @@
+from src.binary_converter import to_binary
+
+def test_to_binary(mocker) -> None:
+    assert to_binary(0) == 0
+    assert to_binary(1) == 1
+    assert to_binary(2) == 10
+    assert to_binary(1234) == 10011010010
