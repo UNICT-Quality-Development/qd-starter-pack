5b4467a4039f12a6bbfc823886881badc1ec3141
Luca Strano <stranoluca469955@gmail.com>
2022-11-16 21:52:41 +0100

    fix: remove unnecessary variables

--- src/verify.py
+++ src/verify.py
@@ -4,12 +4,9 @@ def check(n: list, number: int) -> bool:
     return False
 
 def main(N: list, number: int) -> str:
-    flag = check(N, number)
-    
-    if flag is True: 
+    if check(N, number) is True: 
         return f"The number {number} is present in the array"
-    else:
-        return f"The number {number} is not present in the array"
+    return f"The number {number} is not present in the array"
 
 if __name__ == "__main__":
     N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
