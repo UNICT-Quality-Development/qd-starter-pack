a92a2f34ccdc8ff4ab83fbb94b201e7e0f5b2a5c
Andrei Stefan <andry4you@hotmail.it>
2022-11-17 00:12:54 +0100

    test of generate function

--- tests/test_random.py
+++ tests/test_random.py
@@ -1,4 +1,17 @@
 from src.random_number import generate
+from pytest_mock import MockerFixture
+from src import random_number
 
-def test_random() -> None:
-    assert generate() <= 10 and generate() >= 1
+def test_random(mocker: MockerFixture) -> None:
+    # arrange
+    mock_random_return = 4
+    mocker.patch.object(random_number, "generate", return_value=mock_random_return)
+    spy=mocker.spy(random_number,"generate")
+    
+    # act
+    res = random_number.generate()
+
+    # assert
+    assert res <= 10 and res >= 1
+    assert spy.call_count == 1
+    assert spy.spy_return == mock_random_return
