4ed29003fa75bfa00b759b5178548f023a8e861e
Giancarmelo <giancarmelo99@gmail.com>
2022-11-16 18:11:44 +0100

    chore: remove unused code

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,13 +1,4 @@
 from src.verify import verify
-import random
-
-def genRandomList() -> list[int]:
-  randomlist = []
-  for i in range(0,5):
-    n = random.randint(1,30)
-  randomlist.append(n)
-  return randomlist
-
 
 def test_verify() -> None:
   assert verify([2,3,4,5,6,8], 7) is False
