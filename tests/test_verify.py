77047e8abc3cc7752b791396c858b248892a3a79
Andrei Stefan <andry4you@hotmail.it>
2022-11-17 00:17:15 +0100

    fix: deleted re-definition of list_numbers

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,6 +1,5 @@
 from src.verify import verify, list_numbers
 
 def test_verify() -> None:
-    list_numbers = ('1','2','3','4','5','6','7','8','9','10')
     assert verify(list_numbers, '4') == ""
     assert verify(list_numbers, '14') == " not"
