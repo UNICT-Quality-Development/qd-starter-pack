91c7df41035b23c14b02df22a69130253c8f177a
Luca Strano <stranoluca469955@gmail.com>
2022-11-16 20:49:35 +0100

    fix: modify verify.py

--- src/verify.py
+++ src/verify.py
@@ -1,17 +1,18 @@
-def verify(n: list, number: int) -> bool:
+def check(n: list, number: int) -> bool:
     if number in n:
         return True
     return False
 
-def main():
-    N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
-
-    number = int(input("Insert number: "))
-
-    if verify(N, number) is True: 
-        print(f"The number {number} is present in the array")
+def main(N: list, number: int) -> str:
+    flag = check(N, number)
+    
+    if flag is True: 
+        return f"The number {number} is present in the array"
     else:
-        print(f"The number {number} is not present in the array")
+        return f"The number {number} is not present in the array"
 
 if __name__ == "__main__":
-   main()
+    N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
+    number = int(input("Insert number: "))
+
+    print(main())
