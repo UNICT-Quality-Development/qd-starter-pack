633fa553601148c9d891d3011fdd3818f9b941c7
Luca Strano <stranoluca469955@gmail.com>
2022-11-16 21:41:13 +0100

    test: add test main 2

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,3 +1,4 @@
+import src.verify
 from src.verify import check
 from src.verify import main
 from pytest_mock import MockerFixture
@@ -7,13 +8,26 @@ def test_verify():
     assert check(N, 4) == True
     assert check(N, 10) == False
 
-def test_main(mocker: MockerFixture):
+def test_main_a(mocker: MockerFixture):
     N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
     mock_verify_return = True
     mocker.patch(__name__ + '.check', return_value = mock_verify_return)
+    spy = mocker.spy(src.verify, "check")
+    
+    res = main(N, 4)
 
-    res1 = main(N, 4)
-    res2 = main(N, 10)
+    assert res == "The number 4 is present in the array"
+    assert spy.call_count == 1
+    assert spy.spy_return == mock_verify_return
 
-    assert res1 == "The number 4 is present in the array"
-    assert res2 == "The number 10 is not present in the array"
+def test_main_b(mocker: MockerFixture):
+    N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
+    mock_verify_return = False
+    mocker.patch(__name__ + '.check', return_value = mock_verify_return)
+    spy = mocker.spy(src.verify, "check")
+
+    res = main(N, 10)
+
+    assert res == "The number 10 is not present in the array"
+    assert spy.call_count == 1
+    assert spy.spy_return == mock_verify_return
