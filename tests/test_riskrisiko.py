d099dd010cbe57f6884926adc9f83984147a55b3
Melo Fuccio <carmelofuccio22@gmail.com>
2022-11-17 16:36:37 +0100

    add: test_tuple_dices_1

--- tests/test_riskrisiko.py
+++ tests/test_riskrisiko.py
@@ -1,7 +1,6 @@
-from src.riskrisiko import throwing_dice, random
 from pytest_mock import MockerFixture
 
-
+from src.riskrisiko import random, throwing_dice, tuple_dices
 
 
 def test_throwing_dice_1(mocker: MockerFixture) -> None:
@@ -12,8 +11,8 @@ def test_throwing_dice_1(mocker: MockerFixture) -> None:
     res = throwing_dice()
     
     assert res == 0
-    assert spy.call_count == 1
     assert spy.spy_return == mock_random_return
+    spy.assert_called_once_with(1,6)
     
  
 def test_throwing_dice_2(mocker: MockerFixture) -> None:
@@ -23,23 +22,19 @@ def test_throwing_dice_2(mocker: MockerFixture) -> None:
     
      res = throwing_dice()
     
+    
      assert res == 7
      assert spy.call_count == 1
      assert spy.spy_return == mock_random_return
+     
+def test_tuple_dices_1(mocker: MockerFixture) -> None:
+    mock_random_return = 0
+    mocker.patch.object(random,"randint",return_value=mock_random_return)
+    spy = mocker.spy(random,"randint")
     
-def test_throwing_dice_3(mocker: MockerFixture) -> None:
-     mock_a_return = True
-     mocker.patch(__name__+'randint',return_value=mock_a_return)
-     spy = mocker.spy(random,"randint")
+    res = tuple_dices("name")
     
-     res = throwing_dice()
+    assert res == [0,0,0]
+    assert spy.call_count == 3
+    assert spy.spy_return == mock_random_return
     
-     assert res == [1,2,3,4,5,6]
-     assert spy.call_count == 7
-     assert spy.spy_return == mock_a_return
-
-    
-# if __name__  == "__main__":
-#     a_test_throwing_dice_1()
-#     a_test_throwing_dice_2()
-#     b_test_throwing_dice_1()
