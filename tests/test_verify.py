de06886c9688ed35f4b0dd01d841a7041862d9ad
Luca Strano <stranoluca469955@gmail.com>
2022-11-17 16:54:02 +0100

    tests: add some tests

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -4,20 +4,23 @@ from src.verify import main
 from pytest_mock import MockerFixture
 
 N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
+test_a = 4
+test_b = 10
 
 def test_verify():
-    assert check(N, 4)
-    assert check(N, 10) == False
+    assert check(N, test_a)
+    assert check(N, test_b) == False
 
 def test_main_a(mocker: MockerFixture):
     mock_verify_return = True
     mocker.patch(__name__ + '.check', return_value = mock_verify_return)
     spy = mocker.spy(src.verify, "check")
     
-    res = main(N, 4)
+    res = main(N, test_a)
 
     assert res == "The number 4 is present in the array"
-    assert spy.call_count == 1
+    #assert spy.call_count == 1
+    spy.assert_called_once_with(N, test_a)
     assert spy.spy_return == mock_verify_return
 
 def test_main_b(mocker: MockerFixture):
@@ -25,8 +28,8 @@ def test_main_b(mocker: MockerFixture):
     mocker.patch(__name__ + '.check', return_value = mock_verify_return)
     spy = mocker.spy(src.verify, "check")
 
-    res = main(N, 10)
+    res = main(N, test_b)
 
     assert res == "The number 10 is not present in the array"
-    assert spy.call_count == 1
+    spy.assert_called_once_with(N, test_b)
     assert spy.spy_return == mock_verify_return
