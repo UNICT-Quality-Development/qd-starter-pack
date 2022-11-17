f71eb180cb9603dfc4c5cdd2e04830f3f87dfd2c
Luca Strano <stranoluca469955@gmail.com>
2022-11-17 16:53:07 +0100

    fix: resolve pipeline problems

--- src/verify.py
+++ src/verify.py
@@ -1,13 +1,13 @@
-def check(n: list, number: int) -> bool:
-    return number in n
+def check(array: list, x: int) -> bool:
+    return x in array
 
-def main(N: list, number: int) -> str:
-    if check(N, number) is True: 
-        return f"The number {number} is present in the array"
-    return f"The number {number} is not present in the array"
+def main(vett: list, a: int) -> str:
+    if check(vett, a) is True:
+        return f"The number {a} is present in the array"
+    return f"The number {a} is not present in the array"
 
 if __name__ == "__main__":
     N = (3, 4, 5, 1, 2, 3, 4, 9, 13, 0)
     number = int(input("Insert number: "))
 
-    print(main())
+    print(main(N, number))
