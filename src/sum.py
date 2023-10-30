ef2a13e639064aa4ca735920592852aefd560e25
FedericoCannizzaro <fefecnn@gmail.com>
2023-10-30 17:37:16 +0100

    feat:sum done

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,15 @@
-def sum(a: int, b: int) -> int:
-    return a + b
+#Write a program that takes as input two numbers and print the sum.
+#
+#Output:
+#Insert the first number: 1
+#Insert the second number: 2
+#Sum: 3
+
+x = input("Insert the first number: ")
+y = input("Insert the second number: ")
+
+sum = int(x)+int(y)
+
+print("Sum: ", sum)
+
+
