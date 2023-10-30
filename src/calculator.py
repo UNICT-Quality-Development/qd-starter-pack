b969d46252d37500078d20e03f639ca6bcf0af88
FedericoCannizzaro <fefecnn@gmail.com>
2023-10-30 17:52:26 +0100

    feat: calculator done

---
+++ src/calculator.py
@@ -0,0 +1,18 @@
+#Write a program that given two numbers as input make the main operations.
+#
+#Output:
+#Insert first number: 4
+#Insert second number: 2
+#
+#SUM: 6
+#Difference: 2
+#Multiplication: 8
+#Division: 2
+
+x = input("Insert the first number: ")
+y = input("Insert the second number: ")
+
+print ("Sum: ", int(x)+int(y))
+print ("Difference: ", int(x)-int(y))
+print ("Multiplication: ", int(x)*int(y))
+print ("Division: ", int(x)/int(y))
