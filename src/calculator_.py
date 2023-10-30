d6a678d06d3976e5baa1079b72ec66ee9f4b71b8
linuchs <allegrarex#libero.it>
2023-10-30 17:47:04 +0100

    feat: Calculator in python

---
+++ src/calculator_.py
@@ -0,0 +1,19 @@
+"""
+  Write a program that given two numbers as input make the main operations.
+
+  Output:
+  Insert first number: 4
+  Insert second number: 2
+
+  SUM: 6
+  Difference: 2
+  Multiplication: 8
+  Division: 2
+"""
+x=int(input("Inserisci il primo numero: "))
+y=int(input("inserisci il secondo numero: "))
+
+print("Somma tra x e y :",x+y)
+print("Differenza tra x e y :",x-y)
+print("Moltiplicazione tra x e y :",x*y)
+print("Differenza tra x e y:",x/y)
