ccbf37332f1fe6bf8d4d8cc2d32cd9c629fb11d2
Gabriele Privitera <gabriele.pr.01@gmail.com>
2023-10-30 17:43:16 +0100

    fix: fixed pipeline's errors

--- src/sum.py
+++ src/sum.py
@@ -1,12 +1,12 @@
 # Write a program that takes as input two numbers and print the sum.
-
+#
 # Output:
 # Insert the first number: 1
 # Insert the second number: 2
 # Sum: 3
 
 def sum(a: int, b: int) -> int:
-    return a + b
+  return a + b
 
 # Input the first number
 num1 = int(input("Insert the first number: "))
@@ -14,5 +14,5 @@ num1 = int(input("Insert the first number: "))
 # Input the second number
 num2 = int(input("Insert the second number: "))
 
-print("Sum:", sum(num1, num2));
+print("Sum:", sum(num1, num2))
 
