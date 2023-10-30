461fbdc2e60ef28a0091cbc9be77b14ea47e18af
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-30 17:22:17 +0100

    refactor: change variables name

--- src/calculator.py
+++ src/calculator.py
@@ -9,12 +9,12 @@
 #   Multiplication: 8
 #   Division: 2
 
-x = int(input("Insert first number: "))
-y = int(input("Insert second number: "))
-print("SUM: ", x+y)
-print("Difference: ", x-y)
-print("Multiplication: ", x*y)
+first_number = int(input("Insert first number: "))
+second_number = int(input("Insert second number: "))
+print("SUM: ", first_number+second_number)
+print("Difference: ", first_number-second_number)
+print("Multiplication: ", first_number*second_number)
 try:
-    print("Division: ", x/y)
+    print("Division: ", first_number/second_number)
 except ZeroDivisionError:
     print("You cannot divide by 0")
