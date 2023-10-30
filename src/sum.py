43be62627c3d5c03c3fadd2ab929000c84dd0b25
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-30 17:55:01 +0100

    feat: sum exercise

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,9 @@
+#   Write a program that takes as input two numbers and print the sum.
+
+#   Output:
+#   Insert the first number: 1
+#   Insert the second number: 2
+#   Sum: 3
+
 def sum(a: int, b: int) -> int:
     return a + b
