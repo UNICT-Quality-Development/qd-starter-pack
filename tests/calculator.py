ea017741596b2aff749bf02db013a83b4d7adba2
Samuele Leonardi <samueleleoonardi14@gmail.com>
2023-10-30 18:27:59 +0100

    Created a new file named calculator.py

---
+++ tests/calculator.py
@@ -0,0 +1,7 @@
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+
+print("\nThe sum is:",int(x) + int(y))
+print("The difference is:", int(x)- int(y))
+print("The multiplication is: ", int(x) * int(y))
+print("The division is: ", float(x)/float(y))
---
+++ tests/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+print("Insert the first number:")
+x:int = input()
+
+print("Insert the second number")
+y = input()
+
+z:int = x + y
+
+print("Sum is", z )
