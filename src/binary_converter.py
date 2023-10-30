fbeae47b56750043d1fc10cf35223c67715720af
Enrico <henriskdusk@gmail.com>
2023-10-30 18:19:14 +0100

    chore: refactor yet again

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -1,10 +1,9 @@
-def print_binary_number(n: int):
+def print_binary_number(n: int) ->None:
     if n<=1:
         print(n, end="")
         return
     print_binary_number(n//2)
     print(n&1, end="")
-    return
 
 k=int(input("Inserisci un numero\n"))
 print_binary_number(k)
