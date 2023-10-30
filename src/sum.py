d649d2b73e6638c48b489cebe03b8128223be3d3
Matilde Margarone <mtldmrgrn@gmail.com>
2023-10-30 19:43:24 +0100

    feat: sum

--- src/sum.py
+++ src/sum.py
@@ -1,8 +1,8 @@
 def sum(a: int, b: int) -> int:
     return a + b
 
-a= int(input ("Enter the first number: "))
-b= int(input ("Enter the second number: "))
+c= int(input ("Enter the first number: "))
+d= int(input ("Enter the second number: "))
 
-result= sum(a,b)
+result= sum(c,d)
 print (f"The sum is {result} ")
