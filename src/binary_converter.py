bd5ab84cf0c6e0753c8c92da0c704f96bfe1b42a
FedericoCannizzaro <fefecnn@gmail.com>
2023-10-30 18:41:24 +0100

    feat: binary_converter edit

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -1,8 +1,8 @@
 def BinaryConverter(num : int) -> int:
 
-   if num > 1:
-      BinaryConverter(num // 2)
-   print (num % 2, end = '')
+    if num > 1:
+        BinaryConverter(num // 2)
+    print (num % 2, end = '')
 
 x = input("Insert the first number: ")
 print ("The binary number is: ", end='')
