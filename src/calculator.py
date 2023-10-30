0865cd29640906938f46672530fc11ec8fd9478f
Gabriele Privitera <gabriele.pr.01@gmail.com>
2023-10-30 17:34:33 +0100

    feat: add calculator.py

---
+++ src/calculator.py
@@ -0,0 +1,26 @@
+# Write a program that given two numbers as input make the main operations.
+
+# Output:
+# Insert first number: 4
+# Insert second number: 2
+
+# SUM: 6
+# Difference: 2
+# Multiplication: 8
+# Division: 2
+
+def calculator(a: float, b: float):
+  print("Sum: ", a + b);
+  print("Difference: ", a - b);
+  print("Multiplication: ", a * b);
+  print("Division: ", a / b);
+
+# Input the first number
+a = float(input("Insert the first number: "))
+
+# Input the second number
+b = float(input("Insert the second number: "))
+
+calculator(a, b);
+
+
