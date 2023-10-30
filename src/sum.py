c72f9cf79cd88c34925211b95d19f3abddfcb650
linuchs <allegrarex#libero.it>
2023-10-30 18:22:08 +0100

    feat: Sum in python

--- src/sum.py
+++ src/sum.py
@@ -1,2 +1,14 @@
-def sum(a: int, b: int) -> int:
-    return a + b
+"""  Write a program that takes as input two numbers and print the sum.
+
+  Output:
+  Insert the first number: 1
+  Insert the second number: 2
+  Sum: 3
+"""
+def func_somma(var_a: int, var_b: int)-> int:
+    return var_a+var_b
+
+x=int(input("Insert the first number: "))
+y=int(input("Insert the second number: "))
+
+print("Sum: ",func_somma(x,y))
