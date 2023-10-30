611fd43ea913dbe849d5b4992e3550cf06165ae7
Manuel Zuccaro <gerard3599@gmail.com>
2023-10-30 19:00:59 +0100

    refactor: verify

--- src/verify.py
+++ src/verify.py
@@ -1,11 +1,8 @@
-def verifica_presenza_numero(array, numero_da_verificare):
-    if numero_da_verificare in array:
-        return True
-    else:
-        return False
+def verifica_presenza_numero(array, numero_da_verificare) -> bool:
+    return numero_da_verificare in array
 
 numeri = [1, 2, 3, 4, 5, 69]
-numero_cercato = 3
+numero_cercato = 4
 
 if verifica_presenza_numero(numeri, numero_cercato):
     print(f"Il numero {numero_cercato} è presente nell'array.")
