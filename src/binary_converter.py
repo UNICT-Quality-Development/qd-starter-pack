97adab66b90148fe0f4e9c924c639e0a6df99f9e
Enrico <henriskdusk@gmail.com>
2023-10-30 18:06:17 +0100

    chore: refactor code

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -1,7 +1,7 @@
 def print_binary_number(n: int):
-    if(n<=1):
-            print(n, end="")
-            return
+    if n<=1:
+        print(n, end="")
+        return
     print_binary_number(n//2)
     print(n&1, end="")
     return
