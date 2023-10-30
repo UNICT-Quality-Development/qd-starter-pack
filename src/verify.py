c84f3e097666b4c1d45cd2d5662208eebc8f4eaa
Irial McKeown <irialcp@gmail.com>
2023-10-30 18:44:03 +0100

    feat: cambio variabili

--- src/verify.py
+++ src/verify.py
@@ -1,20 +1,20 @@
 import random
 
-def genera_array_numeri_random(n, minimo, massimo):
-    return [random.randint(minimo, massimo) for _ in range(n)]
+def genera_array_numeri_random(numero_elementi, valore_minimo, valore_massimo):
+    return [random.randint(valore_minimo, valore_massimo) for _ in range(numero_elementi)]
 
-n = 10  # Numero di elementi nell'array
-minimo = 1  # Il valore minimo che può essere generato
-massimo = 15  # Il valore massimo che può essere generato
+numero_elementi = 10  # Numero di elementi nell'array
+valore_minimo = 1  # Il valore minimo che può essere generato
+valore_massimo = 15  # Il valore massimo che può essere generato
 
-array_numeri_random = genera_array_numeri_random(n, minimo, massimo)
+array_numeri_random = genera_array_numeri_random(numero_elementi, valore_minimo, valore_massimo)
 
 numero_da_cercare = int(input('Inserisci il numero da cercare: '))
 
 trovato = False
 
-for num in array_numeri_random:
-    if num == numero_da_cercare:
+for numero in array_numeri_random:
+    if numero == numero_da_cercare:
         trovato = True
         break
 
