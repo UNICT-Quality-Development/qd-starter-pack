1abc221fe0ec3ab64ffddbb12dee122b3baa999d
Gabriele Privitera <gabriele.pr.01@gmail.com>
2023-10-30 18:09:40 +0100

    feat: upload random_number.py

---
+++ src/random_number.py
@@ -0,0 +1,12 @@
+# Write a program that generates a random number.
+#
+# Output:
+# The random number is: 4
+
+import random
+
+# Generate a random number between 1 and 10 (you can adjust the range as needed)
+random_number = random.randint(1, 10)
+
+# Print the random number
+print("The random number is:", random_number)
