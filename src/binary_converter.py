7e9d13f08b1bab63c3cf910027fc7e76f6ba427f
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-30 17:18:13 +0100

    feat: binary_converter exercise

---
+++ src/binary_converter.py
@@ -0,0 +1,8 @@
+#   Write a program that given a number as input convert it in binary.
+
+#   Output:
+#   Insert first number: 8
+#   The binary number is: 1000
+
+x = int(input("Insert first number: "))
+print("The binary number is: " + bin(x)[2:])
