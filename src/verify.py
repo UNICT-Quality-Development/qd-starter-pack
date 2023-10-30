4c44f89fe33963e4d090b78cc3edc8cf4fbb39f8
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-30 18:02:07 +0100

    feat: verify exercise

---
+++ src/verify.py
@@ -0,0 +1,17 @@
+# Write a software that verifies if a number is present in a pre-defined array.
+
+# Output example:
+# Insert number 3
+# The number 3 is [not] present in the array.
+
+def isPresent(N: list, n: int) -> bool:
+    return n in N
+
+def main():
+    n = int(input("Insert number: "))
+    N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+    if isPresent(N, n): print(f"The number {n} is present in the array.") 
+    else: print(f"The number {n} is not present in the array.")
+
+if __name__ == "__main__":
+    main()
