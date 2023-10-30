1fa0ebb7650367b66d73038fb7e575584bd5b8af
MatteoP <mlfplumari@gmail.com>
2023-10-30 18:18:12 +0100

    fix: calculator

--- src/calculator.py
+++ src/calculator.py
@@ -13,12 +13,19 @@
 
 
 val_1 = int(input('insert first number: '))
+
 val_2=  int(input ('insert second number: '))
 
+
+
 sum=val_1+val_2
+
 dif=val_1-val_2
+
 mul=val_1*val_2
+
 div=float(val_1/val_2)
 
-print('\nSum: ', SUM,'\nDifference: ', dif)
+print('\nSum: ', sum,'\nDifference: ', dif)
+
 print('\nMultiplication', mul,'\nDivision', div)
