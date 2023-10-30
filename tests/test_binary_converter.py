b4aa065f6800199c6bf2e5d7b9cd5e3a2d28e2e8
Darakuu <elviost@gmail.com>
2023-10-30 18:04:03 +0100

    sistema indentazioni etc...

--- tests/test_binary_converter.py
+++ tests/test_binary_converter.py
@@ -1,6 +1,7 @@
 from src.binary_converter import binary_converter
 
+
 def test_binary_converter() -> None:
-    assert binary_converter(7) == '111'
-    assert binary_converter(13) == '1101'
-    assert binary_converter(128) == '10000000'
+    assert binary_converter(7) == "111"
+    assert binary_converter(13) == "1101"
+    assert binary_converter(128) == "10000000"
