d345bfc7abc711bf742b57ff685b75c3fbb0799e
Darakuu <elviost@gmail.com>
2023-10-30 17:57:57 +0100

    feat: aggiunge un semplice test con tre asserzioni per il binary converter

---
+++ tests/test_binary_converter.py
@@ -0,0 +1,6 @@
+from src.binary_converter import binary_converter
+
+def test_binary_converter() -> None:
+    assert binary_converter(7) == '111'
+    assert binary_converter(13) == '1101'
+    assert binary_converter(128) == '10000000'
