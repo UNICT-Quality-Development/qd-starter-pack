71793095190e14ee5220318c3e1e2b3baa09a808
linuchs <allegrarex#libero.it>
2023-10-30 22:44:53 +0100

    feat: Sum in python - modifica

--- src/sum.py
+++ src/sum.py
@@ -1,14 +1,13 @@
-"""  Write a program that takes as input two numbers and print the sum.
-
+"""
+Write a program that takes as input two numbers and print the sum.
   Output:
   Insert the first number: 1
   Insert the second number: 2
   Sum: 3
 """
-def func_somma(var_a: int, var_b: int)-> int:
-    return var_a+var_b
+def sum(var_a: int, var_b: int)-> int:
+    return var_a + var_b
 
 x=int(input("Insert the first number: "))
 y=int(input("Insert the second number: "))
-
-print("Sum: ",func_somma(x,y))
+print("Sum: ",sum(x,y))
