13dae4a414c018acc4b402333591932a55ebab7b
Lorenzo <lorenzodidome03@gmail.com>
2023-10-30 18:12:09 +0100

    feat:completed ex4

---
+++ src/ex4.py
@@ -0,0 +1,33 @@
+def func()->None:
+    print("Enter month number(1-12):")
+     
+    k = int(input())
+    match k:
+        case 1:
+            print("31 days")
+        case 2:
+            print("28/29 days")
+        case 3:
+            print("31 days")
+        case 4:
+            print("30 days")
+        case 5:
+            print("31 days")
+        case 6:
+            print("30 days")
+        case 7:
+            print("31 days")
+        case 8:
+            print("31 days")
+        case 9:
+            print("30 days")
+        case 10:
+            print("31 days")
+        case 11:
+            print("30 days")
+        case 12:
+            print("31 days")
+        case _:
+            print("error ")
+        
+func()
