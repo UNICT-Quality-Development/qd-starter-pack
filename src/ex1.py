2dbe08c36ee3674ec53bc04c3b81d8f9319b4d5e
Lorenzo <lorenzodidome03@gmail.com>
2023-10-30 17:25:29 +0100

    feat:completed ex1

---
+++ src/ex1.py
@@ -0,0 +1,26 @@
+
+
+
+def func(a:int)->int:
+    match a:
+        case 1:
+             print("Monday")
+        case 2:
+            print("Tuesday")
+        case 3:
+            print("Wednesady")
+        case 4:
+            print("Thursday")
+        case 5:
+            print("Friday")
+        case 6:
+            print("Saturday")
+        case 7:
+            print("Sunday")
+        case _:
+           print("numero inserito errato")
+        
+
+print("inserisci numero da 1 a 7:")
+k = int(input())
+func(k)
