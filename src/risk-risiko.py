6a897be127211c086a9f14b7dc57887600e68cfc
Riccardo Rubino <Ricky.Rubino@yahoo.it>
2023-10-30 18:36:19 +0100

    feat: added random numbers between 1 and 6 to both players dice-arrays and implemented function that prints results

---
+++ src/risk-risiko.py
@@ -0,0 +1,30 @@
+import random
+
+def randDice():
+    return random.randint(1, 6)
+
+def printDices(dices):
+    print(dices[0], "(N)")
+    print(dices[1], "(M)")
+    print(dices[2], "(0)")
+
+def printResults(red, blue):
+    print("\n  R    B")
+    print("N", red[0], "vs", blue[0], "=>", 'red' if red[0] > blue[0] else 'blue', "wins")
+    print("M", red[1], "vs", blue[1], "=>", 'red' if red[1] > blue[1] else 'blue', "wins")
+    print("O", red[2], "vs", blue[2], "=>", 'red' if red[2] > blue[2] else 'blue', "wins")
+
+redDices = [randDice(), randDice(), randDice()]
+blueDices = [randDice(), randDice(), randDice()]
+
+redDices.sort(reverse = True)
+blueDices.sort(reverse = True)
+
+print("\nRed dices:")
+printDices(redDices)
+
+print("\nBlue dices:")
+printDices(blueDices)
+
+printResults(redDices, blueDices)
+
