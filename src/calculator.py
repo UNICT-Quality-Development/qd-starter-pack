2ed0d84046366fe95cf0109ddbb0642c1d9490a7
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-30 17:13:37 +0100

    feat: calculator exercise

---
+++ src/calculator.py
@@ -0,0 +1,20 @@
+#   Write a program that given two numbers as input make the main operations.
+
+#   Output:
+#   Insert first number: 4
+#   Insert second number: 2
+
+#   SUM: 6
+#   Difference: 2
+#   Multiplication: 8
+#   Division: 2
+
+x = int(input("Insert first number: "))
+y = int(input("Insert second number: "))
+print("SUM: ", x+y)
+print("Difference: ", x-y)
+print("Multiplication: ", x*y)
+try:
+    print("Division: ", x/y)
+except ZeroDivisionError:
+    print("You cannot divide by 0")
