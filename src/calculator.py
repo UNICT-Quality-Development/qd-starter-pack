6454d3c79c0f33d53379e42e98bf31f2b8723a20
Darakuu <elviost@gmail.com>
2023-10-30 18:09:04 +0100

    feat: implementa un semplicissimo calculator con 5 funzioni di base
    le funzioni sono somma, sottrazione, moltiplicazione, divisione e modulo

---
+++ src/calculator.py
@@ -0,0 +1,18 @@
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+def sub(a: int, b: int) -> int:
+    return a - b
+
+
+def mul(a: int, b: int) -> int:
+    return a * b
+
+
+def div(a: int, b: int) -> int:
+    return a / b
+
+
+def mod(a: int, b: int) -> int:
+    return a % b
