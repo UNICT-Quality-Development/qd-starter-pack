d064409ca81fe84d6cb63e3a4049c45c3089aa2e
LorenzoMaci <lorenzomaci@virgilio.it>
2023-10-30 17:56:25 +0100

    verify

--- src/verify.py
+++ src/verify.py
@@ -4,20 +4,20 @@
 #The number 3 is [not] present in the array.
 
 def main():
-  N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
-  numero_da_trovare = int(input("Insert number: "))
-  found = False
+    N = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
+    numero_da_trovare = int(input("Insert number: "))
+    found = False
 
-  for num in N:
-    if num == numero_da_trovare:
-      found = True
-      break
+    for num in N:
+        if num == numero_da_trovare:
+            found = True
+            break
   
-  if found:
-    print(f"The number {numero_da_trovare} is present in the array.")
-  else:
-    print(f"The number {numero_da_trovare} is [not] present in the array.")
+    if found:
+        print(f"The number {numero_da_trovare} is present in the array.")
+    else:
+        print(f"The number {numero_da_trovare} is [not] present in the array.")
 
 
 if __name__ == "__main__":
-  main()
+    main()
