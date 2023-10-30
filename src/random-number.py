e519bd9deaa1d5a4b33d446aacec437a9dd5ce40
0xr3z <russoangelo.f@gmail.com>
2023-10-30 18:52:02 +0100

    refactor: lint error

--- src/random-number.py
+++ src/random-number.py
@@ -1,9 +1,6 @@
 import random
-'''
-Write a program that generates a random number.
-
-Output:
-The random number is: 4
-'''
+#Write a program that generates a random number.
+#Output:
+#The random number is: 4
 number = int(random.randrange(0,1000))
 print(f"The random number is: {number}")
