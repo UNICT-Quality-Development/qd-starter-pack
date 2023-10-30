49cda9a9c7b753f8b1ff17c2f64bcd619f0b3d2c
Manuel Zuccaro <gerard3599@gmail.com>
2023-10-30 17:59:14 +0100

    BinaryConverter

---
+++ src/binary_converter.py
@@ -0,0 +1,14 @@
+def decimal_to_binary(n):
+    b = ""
+    while n > 0:
+        if n % 2 == 0:
+            b = '0' + b
+        else:
+            b = '1' + b
+        n //= 2
+    return b
+
+
+n = int(input("Inserisci un numero: "))
+binary_number = decimal_to_binary(n)
+print("Il numero binario è:", binary_number)
