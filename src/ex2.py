ba71b41aa56bfb538ebf22c659af1dddcda3263f
Darakuu <elviost@gmail.com>
2023-10-31 12:50:46 +0100

    style: sostituisce un paio di magic numbers con costant pep8 compliant

--- src/ex2.py
+++ src/ex2.py
@@ -13,7 +13,9 @@ def day_of_the_week():
         "Saturday",
         "Sunday",
     ]
-    if weekDay not in range(1, 8):  # range usa un intervallo [a,b[
+    MIN_NUMBER = 1
+    MAX_NUMBER = 8
+    if weekDay not in range(MIN_NUMBER, MAX_NUMBER):  # range usa un intervallo [a,b[
         print("Error: number not in expected range (1 through 7), retry.")
     else:
         print(f"Selected day: {daysOfTheWeek[weekDay-1]}")
