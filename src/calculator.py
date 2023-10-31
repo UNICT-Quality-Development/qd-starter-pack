95be9b277585159550b46acfcc2319a917be07b6
Darakuu <elviost@gmail.com>
2023-10-31 13:05:38 +0100

    feat: aggiunto un blocco try/except alla divisione, aggiunta una brevissima procedura per testare le cinque operazioni

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,6 @@
+from math import inf
+
+
 def sum(a: int, b: int) -> int:
     return a + b
 
@@ -10,9 +13,24 @@ def mul(a: int, b: int) -> int:
     return a * b
 
 
-def div(a: int, b: int) -> int:
-    return a / b
+def div(a: int, b: int) -> float:
+    try:
+        result = a / b
+    except ZeroDivisionError as e:
+        print("Errore: divisione per zero non definita.")
+        return None  # Ritorna none per error checking
+    return result
 
 
 def mod(a: int, b: int) -> int:
     return a % b
+
+
+num_1 = int(input("Inserisci il primo operando: "))
+num_2 = int(input("Inserisci il secondo operando: "))
+
+print(sum(num_1, num_2))
+print(sub(num_1, num_2))
+print(mul(num_1, num_2))
+print(div(num_1, num_2))
+print(mod(num_1, num_2))
