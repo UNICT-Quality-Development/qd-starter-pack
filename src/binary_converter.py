10b8760df244666abe9e4b95431518cd755dbe41
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-31 18:01:43 +0100

    refactor: remove lambda function and add a normal function for binary conversion

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -4,8 +4,9 @@
 #   Insert first number: 8
 #   The binary number is: 1000
 
-#The lambda function decimal_to_binary takes a decimal number and returns a binary number in which we remove the first 2 characters that identifies a binary digit (0b).
-decimal_to_binary = lambda decimal_number: bin(decimal_number)[2:]
+#The function decimal_to_binary takes a decimal number and returns a binary number in which we remove the first 2 characters that identifies a binary digit (0b).
+def decimal_to_binary(decimal_number):
+    return bin(decimal_number)[2:]
 
 decimal_number = int(input("Insert first number: "))
 
