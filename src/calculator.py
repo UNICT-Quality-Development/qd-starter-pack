3c738003f0c22ea1e934296180337afd96ce5354
Enrico Rizzo <enry.rizzo46@gmail.com>
2023-10-31 20:11:38 +0100

    feat: added comments

--- src/calculator.py
+++ src/calculator.py
@@ -8,4 +8,4 @@
 print("Insert first number:")
 inp_num = input()
 binary_num = bin(int(inp_num))
-print("Output:",binary_num[2:])
+print("Output:",binary_num[2:]) #[2:]--> used to remove the '0b' prefix added by the function bin() after converting input
