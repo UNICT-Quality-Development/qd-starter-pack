d792a2617af4973082dc4bec967d886fae571f1f
Fabryx <fabrizio.cipriano@studium.unict.it>
2023-10-31 10:54:42 +0100

    funzone sum

--- sum.py
+++ sum.py
@@ -1,4 +1,9 @@
 num1=input("Inserisci il primo numero: ")
 num2=input("inserisci il secondo numero: ")
-sum=int(num1)+int(num2)
-print("sum= ",sum)
+
+
+def summ(num1,num2):
+    sum=int(num1)+int(num2)
+    return sum
+
+print("La somma dei numeri è = ",summ(num1,num2))
