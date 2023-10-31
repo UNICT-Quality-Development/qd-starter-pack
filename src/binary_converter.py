f7f69e7d75867dc8900c28b8e00a92665a1b65dd
GianmarcoBasile <basilemarc2000@gmail.com>
2023-10-31 16:43:49 +0100

    refactor: add comment to explain decimal_to_binary function

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -4,5 +4,9 @@
 #   Insert first number: 8
 #   The binary number is: 1000
 
+#The lambda function decimal_to_binary takes a decimal number and returns a binary number in which we remove the first 2 characters that identifies a binary digit (0b).
+decimal_to_binary = lambda decimal_number: bin(decimal_number)[2:]
+
 decimal_number = int(input("Insert first number: "))
-print("The binary number is: " + bin(decimal_number)[2:])
+
+print("The binary number is: " + decimal_to_binary(decimal_number))
