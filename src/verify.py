39aba6d51ff446518a5d5792f65f2a659fca65a9
Enrico Rizzo <enry.rizzo46@gmail.com>
2023-10-31 20:01:04 +0100

    feat: now is possible to use verify as a function and removed some comments

--- src/verify.py
+++ src/verify.py
@@ -6,14 +6,11 @@ Insert number 3
 The number 3 is [not] present in the array.
 """
 array = [4,6,8,2,9]
-print("Insert number")
+print("Insert number:")
 input_int = int(input())
-"""for x in array:
-    if x == input_int:
+
+def verify(input_int):
+   if input_int in array:
        print("The number",input_int,"is present in the array")
-       break
-else:
-    print("The number",input_int,"is not present in the array")"""
-if input_int in array:
-    print("The number",input_int,"is present in the array")
-else: print("The number",input_int,"is not present in the array")
+   else: print("The number",input_int,"is not present in the array")
+verify(input_int)
