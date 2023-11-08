d90d0e76ccbd91f19c36a23e730dd86953dc76cb
Darakuu <elviost@gmail.com>
2023-11-08 18:09:58 +0100

    feat: adds a main function to binary_converter.

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -1,5 +1,9 @@
 def binary_converter(decimal_num: int) -> str:
     converted = ""
+    if decimal_num == 0:
+        return "0"
+    if decimal_num < 0:
+        raise ValueError("Solo numeri positivi sono permessi")
     while decimal_num > 0:
         if decimal_num % 2 == 0:
             converted += "0"  # concateno perché sì. (dai è più compatto)
@@ -9,6 +13,11 @@ def binary_converter(decimal_num: int) -> str:
     return converted[::-1]  # Sì stefano lo so è bruttissima questa cosa <3
 
 
-# Stringhe per testare in loco:
-# num = int(input("Mbare dammi un numero bro forza:"))
-# print(binary_converter(num))
+def main():
+    n = int(input("Inserisci un numero: "))
+    binary_numer = binary_converter(n)
+    print("Il tuo numero in binario è:", binary_numer)
+
+
+if __name__ == "__main__":
+    main()
