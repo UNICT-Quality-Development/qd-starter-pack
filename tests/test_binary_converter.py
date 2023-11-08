8a5d018fec4f66bac44131f634d298125b712a22
Darakuu <elviost@gmail.com>
2023-11-08 18:10:34 +0100

    feat: proper test (maybe?) using pytest fixtures

--- tests/test_binary_converter.py
+++ tests/test_binary_converter.py
@@ -1,7 +1,38 @@
 from src.binary_converter import binary_converter
+import pytest
 
 
-def test_binary_converter() -> None:
-    assert binary_converter(7) == "111"
-    assert binary_converter(13) == "1101"
-    assert binary_converter(128) == "10000000"
+@pytest.fixture
+def mocker():
+    from unittest.mock import Mock
+
+    return Mock()
+
+
+@pytest.fixture
+def binary_converter_function():
+    return binary_converter
+
+
+def test_binary_converter_positive_integer(binary_converter_function):
+    decimal_number = 10
+    binary_result = binary_converter_function(decimal_number)
+    assert binary_result == "1010"
+
+
+def test_binary_converter_zero(binary_converter_function):
+    decimal_number = 0
+    binary_result = binary_converter_function(decimal_number)
+    assert binary_result == "0"
+
+
+def test_binary_converter_negative_integer(binary_converter_function):
+    decimal_number = -5
+    with pytest.raises(ValueError):
+        binary_converter_function(decimal_number)
+
+
+def test_binary_converter_large_integer(binary_converter_function):
+    decimal_number = 1234567890
+    binary_result = binary_converter_function(decimal_number)
+    assert binary_result == "1001001100101100000001011010010"
