a6a0f3a48b0ee6ab803af8993c54955373616279
Matilde Margarone <mtldmrgrn@gmail.com>
2023-11-08 19:08:11 +0100

    feat: main function

--- calculator.py
+++ calculator.py
@@ -10,18 +10,19 @@ def mul(a: int, b: int)-> int:
 def div(a: int, b:int)-> int:
     return a/b
 
-a= int(input ("Enter the first number: "))
-b= int(input ("Enter the second number: "))
+if __name__=="__main__":
+    a= int(input ("Enter the first number: "))
+    b= int(input ("Enter the second number: "))
 
-s= sum(a,b)
-df= diff(a,b)
-m= mul(a,b)
+    s= sum(a,b)
+    df= diff(a,b)
+    m= mul(a,b)
 
-print (f"The sum is {s} ")
-print (f"The difference is {df} ")
-print (f"The multiplication is {m} ")
+    print (f"The sum is {s} ")
+    print (f"The difference is {df} ")
+    print (f"The multiplication is {m} ")
 
-if b==0: print("The division is not possible") 
-else: 
-    dv= div(a,b) 
-    print(f"The division is {dv} ")
+    if b==0: print("The division is not possible") 
+    else: 
+        dv= div(a,b) 
+        print(f"The division is {dv} ")
