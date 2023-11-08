a88fe82a057ff0c0045a5a68679cfbdf9c961ea1
GiuseppeBnn <giusbon2@gmail.com>
2023-11-08 18:41:39 +0100

    changed test array names and exported range in global array

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -4,15 +4,16 @@
 ##Insert number 3
 ##The number 3 is [not] present in the array.
 ##
-test_array = [2,4,6,8,10,12,14,16,18,20]
+success_test_array = [2,4,6,8,10,12,14,16,18,20]
+fail_test_array = range(35,100)
 import sys
 sys.path.append('../src')
 
 import verify
 
 def test_verify():
-    for i in test_array:
-        assert verify.verify(i) == True
-    for i in range(35,55):
-        if i not in test_array:
-            assert verify.verify(i) == False    
+    for i in success_test_array:
+        assert verify.is_element_in_list(i) == True
+    for i in fail_test_array:
+        if i not in success_test_array:
+            assert verify.is_element_in_list(i) == False    
