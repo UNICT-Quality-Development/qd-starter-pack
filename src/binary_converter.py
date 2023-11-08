d56224388449ba9b59b1ffbe7d2382a69e063152
Manuel Zuccaro <gerard3599@gmail.com>
2023-11-08 18:06:02 +0100

    Refactor: sistemato il codice per far funzionare il test

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -1,4 +1,8 @@
 def decimal_to_binary(n):
+    if n < 0:
+        raise ValueError("Il numero deve essere positivo")
+    if n == 0:
+        return "0"
     b = ""
     while n > 0:
         if n % 2 == 0:
@@ -8,7 +12,11 @@ def decimal_to_binary(n):
         n //= 2
     return b
 
+def main():
+    n = int(input("Inserisci un numero: "))
+    binary_number = decimal_to_binary(n)
+    print("Il numero binario è:", binary_number)
 
-n = int(input("Inserisci un numero: "))
-binary_number = decimal_to_binary(n)
-print("Il numero binario è:", binary_number)
+if __name__ == "__main__":
+    main()
+
