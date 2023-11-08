c49c12059c1b699c34d4e047a748b5a39307763d
linuchs <allegrarex#libero.it>
2023-11-08 18:56:14 +0100

    feat: Aggiornamento di calculator e test con pytest

--- src/calculator_.py
+++ src/calculator_.py
@@ -10,15 +10,28 @@
   Multiplication: 8
   Division: 2
 """
-x=""
-y=""
-while x.isdecimal() is False:
-    x=input("Inserisci il primo numero: ")
-while y.isdecimal() is False:
-    y=input("inserisci il secondo numero: ")
-x=int(x)
-y=int(y)
-print("Somma tra x e y :",x+y)
-print("Differenza tra x e y :",x-y)
-print("Moltiplicazione tra x e y :",x*y)
-print("Differenza tra x e y:",x/y)
+
+
+def Somma(a: int, b: int) -> int:
+    return a + b
+def Differenza(a: int, b: int) -> int:
+    return a - b
+def Moltiplicazione(a: int, b: int) -> int:
+    return a * b
+def Divisione(a: int, b: int) -> int:
+    return a / b
+if __name__ == '__main__':
+  x=""
+  y=""
+  while x.isdecimal() is False:
+      x=input("Inserisci il primo numero: ")
+  while y.isdecimal() is False:
+      y=input("inserisci il secondo numero: ")
+  x=int(x)
+  y=int(y)
+  print("Somma tra x e y :",Somma(x,y))
+  print("Differenza tra x e y :",Differenza(x,y))
+  print("Moltiplicazione tra x e y :",Moltiplicazione(x,y))
+  print("Differenza tra x e y:",Divisione(x,y))
+
+
