ba0e8e6583e4d712e6c30fc78ff725711186a054
Lorenzo <lorenzodidome03@gmail.com>
2023-11-08 18:46:35 +0100

    test: implemented test for random exercises

---
+++ tests/testex5.py
@@ -0,0 +1,10 @@
+import src.ex5 as ex5
+import pytest
+from pytest_mock import MockerFixture
+
+
+
+def testex5(mocker : MockerFixture)->None:
+    mocker.patch.object(ex5,"random",return_value = 10)
+
+    assert ex5.func() == 10
