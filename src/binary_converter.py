7ef426c5a2a683c6c100a36326b7775c8bb8dddf
Enrico <henriskdusk@gmail.com>
2023-11-08 18:43:34 +0100

    chore: add initial tests

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -4,7 +4,7 @@ def print_binary_number(n: int) ->None:
         return
     print_binary_number(n//2)
     print(n&1, end="")
-
-k=int(input("Inserisci un numero\n"))
-print_binary_number(k)
-print()
+if __name__ == '__main__':
+	k=int(input("Inserisci un numero\n"))
+	print_binary_number(k)
+	print()
