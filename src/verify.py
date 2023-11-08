cf83ec45b33f074cb7de31c38b8f689fc74f7482
GiuseppeBnn <giusbon2@gmail.com>
2023-11-08 18:40:37 +0100

    changed function name

--- src/verify.py
+++ src/verify.py
@@ -1,6 +1,6 @@
 array = [2,4,6,8,10,12,14,16,18,20]
 
-def verify(number : int) -> bool:
+def is_element_in_list(number : int) -> bool:
     if number in array:
         print("Numero presente")
         return True
@@ -10,5 +10,5 @@ def verify(number : int) -> bool:
 
 if __name__ == "__main__":
     n = int(input("Inserisci numero: "))
-    verify(n)
+    is_element_in_list(n)
 
