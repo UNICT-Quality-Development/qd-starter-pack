1236eaf8ff23b51192d18800ae9f774ba56bcbac
FilippoMarletta <filippo.marletta@ctmobi.it>
2023-11-15 17:51:42 +0100

    refactor(test_is_element_in_list.py): parametrized hard coded values using pytest

--- tests/test_is_element_in_list.py
+++ tests/test_is_element_in_list.py
@@ -1,5 +1,7 @@
 from src.is_element_in_list import is_element_in_list
+import pytest
 
-def test_is_element_in_list() -> None:
-    assert is_element_in_list([1,2,3], 2) == True
-    assert is_element_in_list([1,2,3], 4) == False
+@pytest.mark.parametrize("input_1", [[1, 2, 3]])
+@pytest.mark.parametrize("input_2, expected", [(2, True), (4, False)])
+def test_is_element_in_list(input_1:list, input_2:int, expected:bool) -> None:
+    assert is_element_in_list(input_1, input_2) == expected
