980e2483662d586c5ce16c7c4fac4328b1e1ca67
FilippoMarletta <filippo.marletta@ctmobi.it>
2023-11-15 12:11:15 +0100

    refactor(test_convert_to_binary.py): reduced number of code lines using pytest parametrization

--- tests/test_convert_to_binary.py
+++ tests/test_convert_to_binary.py
@@ -1,14 +1,14 @@
 import src.convert_to_binary as cbt
+import pytest
 from pytest_mock import MockerFixture
 
-def test_convert_to_binary(mocker: MockerFixture) -> None:
+@pytest.mark.parametrize("input,expected,call_counter", [(10,"1010",1), (2.0, "ERRORE",0)])
+def test_convert_to_binary(mocker: MockerFixture, input, expected, call_counter) -> None:
     mock_value = "1010"
     mocker.patch.object(cbt, "bin", return_value = mock_value)
     spy = mocker.spy(cbt, "bin")
     
-    res_1 = cbt.convert_to_binary(10)
-    res_2 = cbt.convert_to_binary(2.0)
-
-    assert res_1 == mock_value
-    assert res_2 == "ERRORE"
-    assert spy.call_count == 1
+    res= cbt.convert_to_binary(input)
+   
+    assert res == expected
+    assert spy.call_count == call_counter
