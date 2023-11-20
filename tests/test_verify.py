54e506fb6ef320908bcf29ef587de02cdf2909ae
Lorenzo <lorenzodidome03@gmail.com>
2023-11-20 17:46:18 +0100

    correction testing file

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -6,8 +6,5 @@ k = [0,1,2,3,4,5,6]
 
 
 def test_verify()->None:
-    #mocker.patch.object(verify,"insert_number",4)
-
-    output = f(k,4)
-    assert output is True
+    assert f(k,4) is True
     assert f(k,10) is False
