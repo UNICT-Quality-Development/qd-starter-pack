129c88d728546385f8745036f1b6858fdd54b97e
Kela Riscica <kela.riscica@gmail.com>
2023-11-21 15:49:03 +0000

    chore: Upload - improve code

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,7 +1,8 @@
 from src.verify import is_num_present
 
 def test_is_num_present() -> None:
-    assert is_num_present(1)
-    assert not is_num_present(60)
-    assert is_num_present(90)
-    assert not is_num_present(1300)
+    list_num_present = [1.00, 90.00, 14.00, 18.25, 19.15, 89.00]
+    assert is_num_present(1, list_num_present)
+    assert not is_num_present(60, list_num_present)
+    assert is_num_present(90, list_num_present)
+    assert not is_num_present(1300, list_num_present)
