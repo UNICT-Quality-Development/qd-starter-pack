a9479b061bcd95a1c147b1ad8e39d68d22861d71
enzo <enzo13617@gmail.com>
2025-11-11 18:39:44 +0100

    feat(ex2.py): exercise done

--- src/ex2.py
+++ src/ex2.py
@@ -1,51 +1,14 @@
-#
-#  Implement the following code in Python replacing if/else if with an array.
-#
-#  Hint:  arr[3] = "Thursday";
-#
+def insert_week_number() -> int:
+    while True:
+        print("insert week number")
+        n_string = input().strip()
+        if not n_string:
+            print("Invalid input! Please enter week number between 1-7")
+        elif n_string.isdigit() and 1 <= int(n_string) <= 7:
+            return int(n_string)
+        else:
+            print("Invalid input! Please enter week number between 1-7")
 
-# #include <iostream>
-# using namespace std;
-#
-# int main()
-# {
-#   int week;
-#
-#   cout << "Enter week number(1-7): " << endl;
-#   cin >> week;
-#
-#   if (week == 1)
-#   {
-#     cout << "Monday" << endl;
-#   }
-#   else if (week == 2)
-#   {
-#     cout << "Tuesday" << endl;
-#   }
-#   else if (week == 3)
-#   {
-#     cout << "Wednesday" << endl;
-#   }
-#   else if (week == 4)
-#   {
-#     cout << "Thursday" << endl;
-#   }
-#   else if (week == 5)
-#   {
-#     cout << "Friday" << endl;
-#   }
-#   else if (week == 6)
-#   {
-#     cout << "Saturday" << endl;
-#   }
-#   else if (week == 7)
-#   {
-#     cout << "Sunday" << endl;
-#   }
-#   else
-#   {
-#     cout << "Invalid input! Please enter week number between 1-7." << endl;
-#   }
-#
-#   return 0;
-# }
+week_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+n_day = insert_week_number()
+print(week_days[n_day - 1])
