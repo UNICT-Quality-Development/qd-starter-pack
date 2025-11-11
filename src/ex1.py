fd2712def8b3105654fd724b792a76c6b5ac0d84
Marco Scalisi <marcoscalisi2702@gamil.com>
2025-11-11 18:14:52 +0100

    ex1_updated_pipeline

--- src/ex1.py
+++ src/ex1.py
@@ -1,19 +1,3 @@
-# Implement this exercise from C++ to Python
-#
-# #include <iostream>
-# using namespace std;
-#
-# int main()
-# {
-#   int week;
-#
-#   /* Input week number from user */
-#   cout << "Enter week number(1-7): " << endl;
-#   cin >> week;
-#
-#   return 0;
-# }
-
 week = int(input("Enter week number (1-7): "))
 
 print("You entered:", week)
@@ -34,6 +18,3 @@ elif week == 7:
     print("Sunday")
 else:
     print("Invalid input! Please enter number between 1-7.")
-
-
-
