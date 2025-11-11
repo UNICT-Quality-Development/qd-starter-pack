b024d7dce996bdcbd5d2f749fdc077d39a0f2a5a
Andrea-Giuseppe-Cavallaro <Adenor88@gmail.com>
2025-11-11 18:11:20 +0100

    feat(calculator): implemented basic operation

--- src/calculator.py
+++ src/calculator.py
@@ -9,4 +9,25 @@
 #   Difference: 2
 #   Multiplication: 8
 #   Division: 2
-# start
+#  
+def sum(a: int, b: int) -> int:
+    return a + b
+
+def sub(a:int, b: int) -> int:
+    return a - b
+
+def mul(a:int, b: int) -> int:
+    return a * b
+
+def div(a:int, b: int) -> int:
+    return "You can't divide by 0" if b == 0 else a /b 
+
+print("Inserisci la x: ")
+x = int(input())
+print("Inserisci la y: ")
+y = int(input())
+
+print(f"sum of {x} and {y} = {sum(x, y)}")
+print(f"subtraction of {x} and {y} = {sub(x, y)}")
+print(f"multiplication of {x} and {y} = {mul(x, y)}")
+print(f"division of {x} and {y} = {div(x, y)}")
