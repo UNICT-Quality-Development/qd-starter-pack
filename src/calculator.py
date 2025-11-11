4e68976a785239d0188ed49b4683ad94c4ba6f31
seby5002 <sebygiuffrida05@gmail.com>
2025-11-11 18:51:36 +0100

    feat: add calculator module

--- src/calculator.py
+++ src/calculator.py
@@ -10,3 +10,27 @@
 #   Multiplication: 8
 #   Division: 2
 #
+
+def sum(a,b):
+    return a+b
+
+def difference(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    if b==0:
+        print("Impossibile eseguire la divisione\n")
+        return
+    return a/b
+
+
+a= int(input("Inserisci il primo numero\n"))
+b= int(input("Inserisci il secondo numero\n"))
+
+print("Somma-> ", sum(a,b),"\n")
+print("Sottrazione-> ", difference(a,b),"\n")
+print("Moltiplicazione-> ", multiplication(a,b),"\n")
+print("Divisione-> ", division(a,b),"\n")
