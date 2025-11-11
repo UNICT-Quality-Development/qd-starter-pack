c49f21ba844939c93327ae6b890a3da064812028
Andrea-Giuseppe-Cavallaro <Adenor88@gmail.com>
2025-11-12 00:03:55 +0100

    style(code): remove obsolete comments

--- src/calculator.py
+++ src/calculator.py
@@ -1,15 +1,3 @@
-#
-#   Write a program that given two numbers as input make the main operations.
-#
-#   Output:
-#   Insert first number: 4
-#   Insert second number: 2
-#
-#   SUM: 6
-#   Difference: 2
-#   Multiplication: 8
-#   Division: 2
-#  
 def sum(a: int, b: int) -> int:
     return a + b
 
