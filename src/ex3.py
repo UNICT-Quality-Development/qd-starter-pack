479c08f4b990fef949a3d33b1dde6a4c74d3f382
Kristoferz <kristianpio.pintaudi@protonmail.com>
2025-11-11 18:45:34 +0100

    es3

--- src/ex3.py
+++ src/ex3.py
@@ -7,37 +7,47 @@
 # {
 #   string textInput;
 #
-#   cout << "Enter a famous name+surname, ex. BarackObama " << endl;
+#   cout << "Enter a famous name+surname, ex. BarackObama ")
 #   cin >> textInput;
 #
 #   if (textInput == "BarackObama")
-#   {
-#     cout << "44th president of the United States" << endl;
-#   }
-#   else if (textInput == "SandroPertini")
-#   {
-#     cout << "Former President of the Italian Republic" << endl;
-#   }
-#   else if (textInput == "NelsonMandela")
-#   {
-#     cout << "Former President of South Africa" << endl;
-#   }
-#   else if (textInput == "MahatmaGandhi")
-#   {
-#     cout << "Bapu" << endl;
-#   }
-#   else if (textInput == "DonaldKnuth")
-#   {
-#     cout << "Creator of LaTeX" << endl;
-#   }
-#   else if (textInput == "DennisRitchie")
-#   {
-#     cout << "Creator of C" << endl;
-#   }
-#   else
-#   {
-#     cout << "Invalid input! Please enter a good name!" << endl;
-#   }
-#
+# #     print(44th president of the United States")
+# # if (textInput == "SandroPertini")
+# #     print(Former President of the Italian Republic")
+# # if (textInput == "NelsonMandela")
+# #     print(Former President of South Africa")
+# # if (textInput == "MahatmaGandhi")
+# #     print(Bapu")
+# # if (textInput == "DonaldKnuth")
+# #     print(Creator of LaTeX")
+# # if (textInput == "DennisRitchie")
+# #     print(Creator of C")
+# #   else
+# #     print(Invalid input! Please enter a good name!")
+# #
 #   return 0;
 # }
+
+good = False
+textInput = input("Enter a famous name+surname, ex. BarackObama ")
+if textInput == "BarackObama":
+    print("44th president of the United States")
+    good = True
+if textInput == "SandroPertini":
+    print("Former President of the Italian Republic")
+    good = True
+if textInput == "NelsonMandela":
+    print("Former President of South Africa")
+    good = True
+if textInput == "MahatmaGandhi":
+    print("Bapu")
+    good = True
+if textInput == "DonaldKnuth":
+    print("Creator of LaTeX")
+    good = True
+if textInput == "DennisRitchie":
+    print("Creator of C")
+    good = True
+
+if good == False:
+    print("Invalid input! Please enter a good name!")
