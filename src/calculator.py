8e88450f1754fe8d4dbb323740b584bdfa547134
zMasterRed <matteosommario@gmail.com>
2025-11-11 18:33:42 +0100

    feat : implemented a basic calculator

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,22 @@
-#
-#   Write a program that given two numbers as input make the main operations.
-#
-#   Output:
-#   Insert first number: 4
-#   Insert second number: 2
-#
-#   SUM: 6
-#   Difference: 2
-#   Multiplication: 8
-#   Division: 2
-#
+def sum(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def divide(a, b):
+    if (b == 0):
+        print("Impossibile dividere per 0")
+    else:
+        return a / b
+            
+a = int(input("Inserisci il primo numero: "))
+b = int(input("Inserisci il secondo numero: "))
+
+print(sum(a,b))
+print(subtraction(a, b))
+print(multiplication(a, b))
+print(divide(a,b))
