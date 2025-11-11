d4570d871f9a987fcbbacede2aa40c73bf0bae32
Carmelo Ferrara <carmelof437@gmail.com>
2025-11-11 17:59:21 +0100

    feat

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -5,3 +5,17 @@
 #   Insert first number: 8
 #   The binary number is: 1000
 #
+
+
+def cast(x) :
+    if (x//2 == 0): return "0"
+
+    if (x % 2 == 1) : return "1" + cast(x//2)
+    return "0" + cast(x//2)
+
+
+x = int(input())
+
+print(cast(x))
+
+
