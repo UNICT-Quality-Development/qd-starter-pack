1d3f9fa72586874301e70a7b8cacf0fb367eb08c
Davide Avellino <d06.avellino@gmail.com>
2025-11-11 18:20:44 +0100

    implemented: week num as input in py

--- src/ex1.py
+++ src/ex1.py
@@ -1,15 +1,21 @@
 # Implement this exercise from C++ to Python
 #
-# #include <iostream>
+#include <iostream>
 # using namespace std;
-#
 # int main()
-# {
+#  {
 #   int week;
 #
-#   /* Input week number from user */
-#   cout << "Enter week number(1-7): " << endl;
-#   cin >> week;
-#
-#   return 0;
+#  /* Input week number from user */
+#  cout << "Enter week number(1-7): " << endl;
+#  cin >> week;
+#  return 0;
 # }
+
+
+
+x = 10
+while(x < 1 or x > 7):
+        x = int(input("enter a week number(1-7): "))
+
+
