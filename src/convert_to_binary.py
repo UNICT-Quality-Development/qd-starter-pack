5dc07d86f23d17789651b2a692f9968fefaf3a98
Andrea Aprile <andy@LenovoAndrea.localdomain>
2025-11-11 17:52:22 +0100

    feat: Add function to convert integer to binary

---
+++ src/convert_to_binary.py
@@ -0,0 +1,12 @@
+#
+#   Write a program that given a number as input convert it in binary.
+#
+#   Output:
+#   Insert first number: 8
+#   The binary number is: 1000
+#
+
+n = int(input("Insert first number: "))
+print("The binary number is: ", bin(n)[2:])
+
+
