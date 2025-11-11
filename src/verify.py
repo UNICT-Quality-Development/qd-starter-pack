a97988753b35664f9855398b3631f17064686fc1
enzo <enzo13617@gmail.com>
2025-11-11 18:05:47 +0100

    fix(verify.py): added input loop

--- src/verify.py
+++ src/verify.py
@@ -2,13 +2,14 @@ def n_in_array(n: int, array: list[int]) -> bool:
     return n in array
 
 def insert_integer() -> int:
-    print("insert a number")
-    n_string = input()
-    try:
-        n = int(n_string)
-        return n
-    except ValueError:
-        print("That's not an int!")
+    while True:
+        print("insert a number")
+        n_string = input()
+        try:
+            n = int(n_string)
+            return n
+        except ValueError:
+            print("That's not an int!")
 
 pre_def_array = [3, 4, 5, 1, 2, 3, 4, 9, 13, 0]
 
