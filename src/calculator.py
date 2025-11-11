0d90e79172842bd6d6bc539cc0f78282802ca637
BecherLiaoumi <Becherliaoumi1@gmail.com>
2025-11-11 18:32:01 +0100

    feat: calcolatrice funzionante

--- src/calculator.py
+++ src/calculator.py
@@ -11,7 +11,26 @@
 #   Division: 2
 #
 
-def sum(x,y): return x + y
-def sum(x,y): return x - y
-def mul(x,y): return x * y
-def div(x,y): return x / y
+def sum(x, y):
+    return "SUM: " + str(x + y)
+
+def sub(x, y):
+    return "Difference: " + str(x - y)
+
+def mul(x, y):
+    return "Multiplication " + str(x * y)
+
+def div(x, y):
+    if y == 0:
+        return "divisione per 0 non permessa"
+    return "Division: " + str(x / y)
+
+
+x = float(input("Insert first number: "))
+y = float(input("Insert second number: "))
+
+print(sum(x, y))
+print(sub(x, y))
+print(mul(x, y))
+print(div(x, y))
+
