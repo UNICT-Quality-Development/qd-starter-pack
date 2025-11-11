afd5490c5192801c52ece0d60ea47d1d9b687507
00Utopia01 <germenaedoardo@gmail.com>
2025-11-11 17:56:26 +0100

    feat: add calculator

--- src/calculator.py
+++ src/calculator.py
@@ -10,3 +10,24 @@
 #   Multiplication: 8
 #   Division: 2
 #
+
+
+print("Insert first number:")
+n1 = input()
+print("Insert second number:")
+n2 = input()
+
+def func(n1,n2):
+    risultati = []
+    risultati.append(int(n1)+int(n2))
+    risultati.append(int(n1)-int(n2))
+    risultati.append(int(n1)*int(n2))
+    risultati.append(int(n1)/int(n2))
+
+    return risultati
+
+phrases = ["Sum:","Difference:", "Multiplication:", "Division:"]
+
+results = func(n1,n2)
+for i in range(4):
+    print(phrases[i] + str(results[i]))
