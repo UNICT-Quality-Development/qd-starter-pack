488ec700d9c46b98267ecba65b4110028097dbdf
Marco Scalisi <marcoscalisi2702@gamil.com>
2025-11-11 17:59:25 +0100

    ex1_updated

--- src/ex1.py
+++ src/ex1.py
@@ -13,3 +13,27 @@
 #
 #   return 0;
 # }
+
+week = int(input("Enter week number (1-7): "))
+
+print("You entered:", week)
+
+if week == 1:
+    print("Monday")
+elif week == 2:
+    print("Tuesday")
+elif week == 3:
+    print("Wednesday")
+elif week == 4:
+    print("Thursday")
+elif week == 5:
+    print("Friday")
+elif week == 6:
+    print("Saturday")
+elif week == 7:
+    print("Sunday")
+else:
+    print("Invalid input! Please enter number between 1-7.")
+
+
+
