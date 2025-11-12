596f26ede5c8d1e86d0efcc5e7ca345b6a4f31e5
Andrea Aprile <andy@LenovoAndrea.localdomain>
2025-11-12 11:07:00 +0100

    feat: add a function that generates a random number from 1 to number

--- src/random_number.py
+++ src/random_number.py
@@ -4,3 +4,10 @@
 #   Output:
 #   The random number is: 4
 #
+import random
+def generator_number():
+ number = int(input("Insert the end of the interval: "))
+ number = random.randint(1, number)
+ print(""The random number between 1 and", n, "is:" ", number)	
+ return number
+generator_number()
