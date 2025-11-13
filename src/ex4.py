75e4a94958205fece843decffa554061b8a85065
Nathh17 <ultrazocker21@gmail.com>
2025-11-13 17:16:48 +0100

    refactor(ex4.py): switched camelCase to snake_case

--- src/ex4.py
+++ src/ex4.py
@@ -55,16 +55,16 @@
 #   return 0;
 # }
 
-def getMonthDays(toFind):
-    monthDays = {
+def get_month_days(to_find):
+    month_days = {
         (1,3,5,7,8,10,12):31,
         (4,6,9,11):30,
         (2,):28
     }
-    for months, days in monthDays.items():
-        if toFind in months:
+    for months, days in month_days.items():
+        if to_find in months:
             return days
     return "Insert valid month"
 
 month = int(input("Enter month number (1-12): "))
-print(getMonthDays(month))
+print(get_month_days(month))
