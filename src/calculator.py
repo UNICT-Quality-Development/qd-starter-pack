76716147d707b0a7ea2bf872207f68b667d6e968
Andrea-Giuseppe-Cavallaro <Adenor88@gmail.com>
2025-11-25 18:36:15 +0100

    feat: calculator unit test

--- src/calculator.py
+++ src/calculator.py
@@ -14,10 +14,11 @@ def div(a: int, b: int) -> float | str:
     return "You can't divide by 0" if b == 0 else a / b
 
 
-x = int(input("Insert first number: "))
-y = int(input("Insert second number: "))
+if __name__ == "__main__":
+    x = int(input("Insert first number: "))
+    y = int(input("Insert second number: "))
 
-print(f"Sum of {x} and {y} = {add(x, y)}")
-print(f"Subtraction of {x} and {y} = {sub(x, y)}")
-print(f"Multiplication of {x} and {y} = {mul(x, y)}")
-print(f"Division of {x} and {y} = {div(x, y)}")
+    print(f"Sum of {x} and {y} = {add(x, y)}")
+    print(f"Subtraction of {x} and {y} = {sub(x, y)}")
+    print(f"Multiplication of {x} and {y} = {mul(x, y)}")
+    print(f"Division of {x} and {y} = {div(x, y)}")
