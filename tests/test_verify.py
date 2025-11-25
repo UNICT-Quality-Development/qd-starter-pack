0932cfa3574d48217eb45d8c68ee1619670f9687
Simone Avellino <s.avellino02@gmail.com>
2025-11-25 18:56:21 +0100

    fix(tests): update assertion style in test_contains function

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -2,7 +2,7 @@ from src.verify import contains
 
 
 def test_contains() -> None:
-    assert contains(5) == False
-    assert contains(1) == True
-    assert contains("1") == False
-    assert contains(1.0) == True
+    assert contains(5) is False
+    assert contains(1) is True
+    assert contains("1") is False
+    assert contains(1.0) is True
