9884c31393f9bbeb3ec25e948deccde8cf0d5226
Carmelo Ferrara <carmelof437@gmail.com>
2025-11-25 18:19:10 +0100

    refactor

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -11,14 +11,19 @@ def cast(x) :
     if (x == 0): return ""
     if (x % 2 == 1) : return cast(x//2) + "1"
     else : return cast(x//2) + "0"
- 
-print("insert a number to cast: ")
-x = float(input())
-if (x<0) :
-    print("1",end = '')
-    x = abs(x) 
-else : print("0",end = '')
-if ((x*10)%10 == 0) : print(cast(x))
-else : print(cast(x)+"."+cast((x*10)%10))
+
+def main() :
+    print("insert a number to cast: ")
+    x = float(input())
+    if (x<0) :
+        print("1",end = '')
+        x = abs(x) 
+    else : print("0",end = '')
+    if ((x*10)%10 == 0) : print(cast(x))
+    else : print(cast(x)+"."+cast((x*10)%10))
+
+if __name__ == "__main__":
+    main()
+
 
 
