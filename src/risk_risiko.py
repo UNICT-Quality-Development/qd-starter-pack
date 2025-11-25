438fc66f1f40eff2744362175b3ebc26e2ea11db
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-25 19:07:28 +0100

    feat(risk_risiko.py test_risk_risiko.py): add test and check the code

---
+++ src/risk_risiko.py
@@ -0,0 +1,71 @@
+#
+#   Write a program that simulates a risk/risiko fight using 6 dices.
+#
+#   How does it work?
+#   When a player attacks another player he uses 3 dices, the red is always the attacker and the blue is the defender.
+#
+#   You have to compare the dice with the highest number to simulate the fight.
+#   N = first highest number
+#   M = second highest number
+#   O = third highest number
+#
+#   If the numbers are equal, the defensor (blue) wins.
+#
+#   Output:
+#   Red dices:
+#   6 (N)
+#   3 (M)
+#   2 (O)
+#
+#   Blue dices:
+#   5 (N)
+#   3 (M)
+#   1 (O)
+#
+#     R    B
+#   N 6 vs 5 => red win
+#   M 3 vs 3 => blue win
+#   O 2 vs 1 => red win
+
+import random
+
+DICES_FOR_PLAYER = 3
+DICES_FACES = 6
+LETTERS = ["N", "M", "O"]
+
+def random_dices_value():
+    return sorted(
+        [random.randint(1, DICES_FACES) for _ in range(DICES_FOR_PLAYER)], reverse=True
+    )
+
+def check_attack(red, blue):
+    blue_winner = 0
+
+    print("\n  R    B")
+    for l_v, r_v, b_v in zip(LETTERS, red, blue):
+        if r_v <= b_v:
+            blue_winner += 1
+            result = "blue Win"
+        else:
+            result = "red Win"
+
+        print(f"{l_v} {r_v} vs {b_v} => {result}")
+
+    return blue_winner
+
+def printWinner(winner: int):
+    if winner > (DICES_FOR_PLAYER / 2):
+        print("Blue Winner")
+    else:
+        print("Red Winner")
+
+if __name__ == "__main__":
+    red_dices = random_dices_value()
+    blue_dices = random_dices_value()
+
+    print("Red Dices: " + str(red_dices))
+    print("Blue DIces: " + str(blue_dices))
+
+    winner = check_attack(red_dices, blue_dices)
+
+    printWinner(winner)
