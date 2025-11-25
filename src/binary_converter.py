096e386fe5aea41c9e9542d1df04b535ef0fb5e8
Lorenzo Comis <lorenzocomis31@gmail.com>
2025-11-25 18:25:37 +0100

    feat: add binary_converter

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -5,3 +5,20 @@
 #   Insert first number: 8
 #   The binary number is: 1000
 #
+
+#  Write a function that given a number as input convert it in binary. dont use bin() function
+def binary_converter(number: int) -> str:
+    if number == 0:
+        return "0"
+    binary_digits = []
+    while number > 0:
+        binary_digits.append(str(number % 2))
+        number //= 2
+    binary_digits.reverse()
+    return ''.join(binary_digits)
+
+if __name__ == "__main__":
+    num = int(input("Insert the number: "))
+    print(f"The binary number is: {binary_converter(num)}")
+    
+    
