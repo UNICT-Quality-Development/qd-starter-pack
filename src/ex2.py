4d5f0e68e366b54cc06fca9a14e21d09a0162c99
LeleValore <emanuelevalore@gmail.com>
2025-11-27 18:22:09 +0100

    feat: implemented ex2.py

--- src/ex2.py
+++ src/ex2.py
@@ -1,51 +1,21 @@
-#
-#  Implement the following code in Python replacing if/else if with an array.
-#
-#  Hint:  arr[3] = "Thursday";
-#
+"""Scrivere in python un esercizio che prenda un numero da 1 a 7 e restituisca il giorno della settimana corrispondente.
+Utilizzare un array per memorizzare i nomi dei giorni della settimana invece di utilizzare una serie di istruzioni if/else if.
+"""
 
-# #include <iostream>
-# using namespace std;
-#
-# int main()
-# {
-#   int week;
-#
-#   cout << "Enter week number(1-7): " << endl;
-#   cin >> week;
-#
-#   if (week == 1)
-#   {
-#     cout << "Monday" << endl;
-#   }
-#   else if (week == 2)
-#   {
-#     cout << "Tuesday" << endl;
-#   }
-#   else if (week == 3)
-#   {
-#     cout << "Wednesday" << endl;
-#   }
-#   else if (week == 4)
-#   {
-#     cout << "Thursday" << endl;
-#   }
-#   else if (week == 5)
-#   {
-#     cout << "Friday" << endl;
-#   }
-#   else if (week == 6)
-#   {
-#     cout << "Saturday" << endl;
-#   }
-#   else if (week == 7)
-#   {
-#     cout << "Sunday" << endl;
-#   }
-#   else
-#   {
-#     cout << "Invalid input! Please enter week number between 1-7." << endl;
-#   }
-#
-#   return 0;
-# }
+# Giorni della settimana memorizzati in un array
+giorni_settimana = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+# Input dell'utente
+numero_settimana = int(input("Enter week number (1-7): "))
+# Verifica se l'input è valido e stampa il giorno corrispondente
+if 1 <= numero_settimana <= 7:
+    print(giorni_settimana[numero_settimana - 1])  # Sottrai 1 per l'indice dell'array
+else:
+    print("Invalid input! Please enter week number between 1-7.")
