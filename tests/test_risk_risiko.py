e7cd97fed54680c744baff3035c815eb04c56ba1
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-27 17:26:12 +0100

    style(test_risk_risiko): fix indentation and spaces

--- tests/test_risk_risiko.py
+++ tests/test_risk_risiko.py
@@ -4,8 +4,7 @@ from src.risk_risiko import check_attack
 def test_check_attack() -> None:
     red_dices = [6, 5, 4]
     blue_dices = [3, 2, 1]
-    
+
     result = check_attack(red_dices, blue_dices)
     assert result is 0
     assert result is not None
-    
