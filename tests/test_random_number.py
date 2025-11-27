702ec3b3db162c86ac2c670c3cdae7c3fbdcf161
Piero <fiorica.gianpiero@gmail.com>
2025-11-27 18:41:11 +0100

    chore: unchained asserts to make the linter happy

--- tests/test_random_number.py
+++ tests/test_random_number.py
@@ -1,4 +1,4 @@
-from src.random_number import *
+from src.random_number import random_positive_int, random_limited_positive_int
 import sys
 import pytest
 
@@ -6,7 +6,8 @@ import pytest
 def test_random_number():
     random_int = random_positive_int()
     assert isinstance(random_int, int)
-    assert random_int >= 0 and random_int <= sys.maxsize
+    assert random_int >= 0
+    assert random_int <= sys.maxsize
 
 
 @pytest.mark.parametrize("upper_limit", [0, -3, sys.maxsize, "a", 4.5, 5000, 1])
